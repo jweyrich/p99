9d3f6ede9e7d489274c8460fdb0b4bcac74434c8
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-02-28 21:18:00 +0100

    add preprocessor decimal <-> hexadecimal conversion

--- include/orwl_macro.h
+++ include/orwl_macro.h
@@ -25,11 +25,11 @@
 #define _INV(N) _variable_argument_list_must_be_divisible_by_ ## N
 
 #define _NARG_64_1(...)                                                 \
-__NARG_64(__VA_ARGS__,                                                  \
-          0x3F, 0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, \
-          0x2F, 0x2E, 0x2D, 0x2C, 0x2B, 0x2A, 0x29, 0x28, 0x27, 0x26, 0x25, 0x24, 0x23, 0x22, 0x21, 0x20, \
-          0x1F, 0x1E, 0x1D, 0x1C, 0x1B, 0x1A, 0x19, 0x18, 0x17, 0x16, 0x15, 0x14, 0x13, 0x12, 0x11, 0x10, \
-          0x0F, 0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00)
+_decimal_(__NARG_64(__VA_ARGS__,                                        \
+                    0x3F, 0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39, 0x38, 0x37, 0x36, 0x35, 0x34, 0x33, 0x32, 0x31, 0x30, \
+                    0x2F, 0x2E, 0x2D, 0x2C, 0x2B, 0x2A, 0x29, 0x28, 0x27, 0x26, 0x25, 0x24, 0x23, 0x22, 0x21, 0x20, \
+                    0x1F, 0x1E, 0x1D, 0x1C, 0x1B, 0x1A, 0x19, 0x18, 0x17, 0x16, 0x15, 0x14, 0x13, 0x12, 0x11, 0x10, \
+                    0x0F, 0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00))
 
 /**
  ** @brief Return the length of the variate argument list.
@@ -49,15 +49,15 @@ __NARG_64(__VA_ARGS__,                                                  \
  ** If the length of the list is odd, a compile time error occurs.
  **/
 #define _NARG_64_2(...)                                                 \
-__NARG_64(__VA_ARGS__,                                                  \
-          _INV(2), 0x1F, _INV(2), 0x1E, _INV(2), 0x1D, _INV(2), 0x1C,   \
-          _INV(2), 0x1B, _INV(2), 0x1A, _INV(2), 0x19, _INV(2), 0x18,   \
-          _INV(2), 0x17, _INV(2), 0x16, _INV(2), 0x15, _INV(2), 0x14,   \
-          _INV(2), 0x13, _INV(2), 0x12, _INV(2), 0x11, _INV(2), 0x10,   \
-          _INV(2), 0x0F, _INV(2), 0x0E, _INV(2), 0x0D, _INV(2), 0x0C,   \
-          _INV(2), 0x0B, _INV(2), 0x0A, _INV(2), 0x09, _INV(2), 0x08,   \
-          _INV(2), 0x07, _INV(2), 0x06, _INV(2), 0x05, _INV(2), 0x04,   \
-          _INV(2), 0x03, _INV(2), 0x02, _INV(2), 0x01, _INV(2), 0x00)
+_decimal_(__NARG_64(__VA_ARGS__,                                        \
+                    _INV(2), 0x1F, _INV(2), 0x1E, _INV(2), 0x1D, _INV(2), 0x1C, \
+                    _INV(2), 0x1B, _INV(2), 0x1A, _INV(2), 0x19, _INV(2), 0x18, \
+                    _INV(2), 0x17, _INV(2), 0x16, _INV(2), 0x15, _INV(2), 0x14, \
+                    _INV(2), 0x13, _INV(2), 0x12, _INV(2), 0x11, _INV(2), 0x10, \
+                    _INV(2), 0x0F, _INV(2), 0x0E, _INV(2), 0x0D, _INV(2), 0x0C, \
+                    _INV(2), 0x0B, _INV(2), 0x0A, _INV(2), 0x09, _INV(2), 0x08, \
+                    _INV(2), 0x07, _INV(2), 0x06, _INV(2), 0x05, _INV(2), 0x04, \
+                    _INV(2), 0x03, _INV(2), 0x02, _INV(2), 0x01, _INV(2), 0x00))
 
 /**
  ** @brief Return the number of triples of the variate argument list.
@@ -67,15 +67,15 @@ __NARG_64(__VA_ARGS__,                                                  \
  ** If the length of the list is not divisible by 3, a compile time error occurs.
  **/
 #define _NARG_64_3(...)                                                 \
-__NARG_64(__VA_ARGS__,                                                  \
-          0x16,                                                         \
-          _INV(3), _INV(3), 0x15, _INV(3), _INV(3), 0x14, _INV(3), _INV(3), 0x13, \
-          _INV(3), _INV(3), 0x12, _INV(3), _INV(3), 0x11, _INV(3), _INV(3), 0x10, \
-          _INV(3), _INV(3), 0x0F, _INV(3), _INV(3), 0x0D, _INV(3), _INV(3), 0x0C, \
-          _INV(3), _INV(3), 0x0B, _INV(3), _INV(3), 0x0A, _INV(3), _INV(3), 0x09, \
-          _INV(3), _INV(3), 0x08, _INV(3), _INV(3), 0x07, _INV(3), _INV(3), 0x06, \
-          _INV(3), _INV(3), 0x05, _INV(3), _INV(3), 0x04, _INV(3), _INV(3), 0x03, \
-          _INV(3), _INV(3), 0x02, _INV(3), _INV(3), 0x01, _INV(3), _INV(3), 0x00)
+_decimal_(__NARG_64(__VA_ARGS__,                                        \
+                    0x16,                                               \
+                    _INV(3), _INV(3), 0x15, _INV(3), _INV(3), 0x14, _INV(3), _INV(3), 0x13, \
+                    _INV(3), _INV(3), 0x12, _INV(3), _INV(3), 0x11, _INV(3), _INV(3), 0x10, \
+                    _INV(3), _INV(3), 0x0F, _INV(3), _INV(3), 0x0D, _INV(3), _INV(3), 0x0C, \
+                    _INV(3), _INV(3), 0x0B, _INV(3), _INV(3), 0x0A, _INV(3), _INV(3), 0x09, \
+                    _INV(3), _INV(3), 0x08, _INV(3), _INV(3), 0x07, _INV(3), _INV(3), 0x06, \
+                    _INV(3), _INV(3), 0x05, _INV(3), _INV(3), 0x04, _INV(3), _INV(3), 0x03, \
+                    _INV(3), _INV(3), 0x02, _INV(3), _INV(3), 0x01, _INV(3), _INV(3), 0x00))
 
 /**
  ** @brief Return the number of quadruples of the variate argument list.
@@ -85,15 +85,15 @@ __NARG_64(__VA_ARGS__,                                                  \
  ** If the length of the list is not divisible by 4, a compile time error occurs.
  **/
 #define _NARG_64_4(...)                                                 \
-__NARG_64(__VA_ARGS__,                                                  \
-          _INV(4), _INV(4), _INV(4), 0xF, _INV(4), _INV(4), _INV(4), 0xE, \
-          _INV(4), _INV(4), _INV(4), 0xD, _INV(4), _INV(4), _INV(4), 0xC, \
-          _INV(4), _INV(4), _INV(4), 0xB, _INV(4), _INV(4), _INV(4), 0xA, \
-          _INV(4), _INV(4), _INV(4), 0x9, _INV(4), _INV(4), _INV(4), 0x8, \
-          _INV(4), _INV(4), _INV(4), 0x7, _INV(4), _INV(4), _INV(4), 0x6, \
-          _INV(4), _INV(4), _INV(4), 0x5, _INV(4), _INV(4), _INV(4), 0x4, \
-          _INV(4), _INV(4), _INV(4), 0x3, _INV(4), _INV(4), _INV(4), 0x2, \
-          _INV(4), _INV(4), _INV(4), 0x1, _INV(4), _INV(4), _INV(4), 0x0)
+_decimal_(__NARG_64(__VA_ARGS__,                                        \
+                    _INV(4), _INV(4), _INV(4), 0xF, _INV(4), _INV(4), _INV(4), 0xE, \
+                    _INV(4), _INV(4), _INV(4), 0xD, _INV(4), _INV(4), _INV(4), 0xC, \
+                    _INV(4), _INV(4), _INV(4), 0xB, _INV(4), _INV(4), _INV(4), 0xA, \
+                    _INV(4), _INV(4), _INV(4), 0x9, _INV(4), _INV(4), _INV(4), 0x8, \
+                    _INV(4), _INV(4), _INV(4), 0x7, _INV(4), _INV(4), _INV(4), 0x6, \
+                    _INV(4), _INV(4), _INV(4), 0x5, _INV(4), _INV(4), _INV(4), 0x4, \
+                    _INV(4), _INV(4), _INV(4), 0x3, _INV(4), _INV(4), _INV(4), 0x2, \
+                    _INV(4), _INV(4), _INV(4), 0x1, _INV(4), _INV(4), _INV(4), 0x0))
 
 #define __NARG_64(...) _ARG_64(__VA_ARGS__)
 
@@ -131,5 +131,160 @@ __NARG_64(__VA_ARGS__,                                                  \
  ** compile time. 
  **/
 #define VA_ARGS(X) size_t X, ...
+
+#define _decimal_0x0 0
+#define _decimal_0x1 1
+#define _decimal_0x2 2
+#define _decimal_0x3 3
+#define _decimal_0x4 4
+#define _decimal_0x5 5
+#define _decimal_0x6 6
+#define _decimal_0x7 7
+#define _decimal_0x8 8
+#define _decimal_0x9 9
+#define _decimal_0xA 10
+#define _decimal_0xB 11
+#define _decimal_0xC 12
+#define _decimal_0xD 13
+#define _decimal_0xE 14
+#define _decimal_0xF 15
+#define _decimal_0x00 0
+#define _decimal_0x01 1
+#define _decimal_0x02 2
+#define _decimal_0x03 3
+#define _decimal_0x04 4
+#define _decimal_0x05 5
+#define _decimal_0x06 6
+#define _decimal_0x07 7
+#define _decimal_0x08 8
+#define _decimal_0x09 9
+#define _decimal_0x0A 10
+#define _decimal_0x0B 11
+#define _decimal_0x0C 12
+#define _decimal_0x0D 13
+#define _decimal_0x0E 14
+#define _decimal_0x0F 15
+#define _decimal_0x10 16
+#define _decimal_0x11 17
+#define _decimal_0x12 18
+#define _decimal_0x13 19
+#define _decimal_0x14 20
+#define _decimal_0x15 21
+#define _decimal_0x16 22
+#define _decimal_0x17 23
+#define _decimal_0x18 24
+#define _decimal_0x19 25
+#define _decimal_0x1A 26
+#define _decimal_0x1B 27
+#define _decimal_0x1C 28
+#define _decimal_0x1D 29
+#define _decimal_0x1E 30
+#define _decimal_0x1F 31
+#define _decimal_0x20 32
+#define _decimal_0x21 33
+#define _decimal_0x22 34
+#define _decimal_0x23 35
+#define _decimal_0x24 36
+#define _decimal_0x25 37
+#define _decimal_0x26 38
+#define _decimal_0x27 39
+#define _decimal_0x28 40
+#define _decimal_0x29 41
+#define _decimal_0x2A 42
+#define _decimal_0x2B 43
+#define _decimal_0x2C 44
+#define _decimal_0x2D 45
+#define _decimal_0x2E 46
+#define _decimal_0x2F 47
+#define _decimal_0x30 48
+#define _decimal_0x31 49
+#define _decimal_0x32 50
+#define _decimal_0x33 51
+#define _decimal_0x34 52
+#define _decimal_0x35 53
+#define _decimal_0x36 54
+#define _decimal_0x37 55
+#define _decimal_0x38 56
+#define _decimal_0x39 57
+#define _decimal_0x3A 58
+#define _decimal_0x3B 59
+#define _decimal_0x3C 60
+#define _decimal_0x3D 61
+#define _decimal_0x3E 62
+#define _decimal_0x3F 63
+#define _decimal_0x40 64
+
+#define __decimal_(HEX) _decimal_ ## HEX
+#define _decimal_(HEX) __decimal_(HEX)
+
+#define _hexadecimal_0 0x00
+#define _hexadecimal_1 0x01
+#define _hexadecimal_2 0x02
+#define _hexadecimal_3 0x03
+#define _hexadecimal_4 0x04
+#define _hexadecimal_5 0x05
+#define _hexadecimal_6 0x06
+#define _hexadecimal_7 0x07
+#define _hexadecimal_8 0x08
+#define _hexadecimal_9 0x09
+#define _hexadecimal_10 0x0A
+#define _hexadecimal_11 0x0B
+#define _hexadecimal_12 0x0C
+#define _hexadecimal_13 0x0D
+#define _hexadecimal_14 0x0E
+#define _hexadecimal_15 0x0F
+#define _hexadecimal_16 0x10
+#define _hexadecimal_17 0x11
+#define _hexadecimal_18 0x12
+#define _hexadecimal_19 0x13
+#define _hexadecimal_20 0x14
+#define _hexadecimal_21 0x15
+#define _hexadecimal_22 0x16
+#define _hexadecimal_23 0x17
+#define _hexadecimal_24 0x18
+#define _hexadecimal_25 0x19
+#define _hexadecimal_26 0x1A
+#define _hexadecimal_27 0x1B
+#define _hexadecimal_28 0x1C
+#define _hexadecimal_29 0x1D
+#define _hexadecimal_30 0x1E
+#define _hexadecimal_31 0x1F
+#define _hexadecimal_32 0x20
+#define _hexadecimal_33 0x21
+#define _hexadecimal_34 0x22
+#define _hexadecimal_35 0x23
+#define _hexadecimal_36 0x24
+#define _hexadecimal_37 0x25
+#define _hexadecimal_38 0x26
+#define _hexadecimal_39 0x27
+#define _hexadecimal_40 0x28
+#define _hexadecimal_41 0x29
+#define _hexadecimal_42 0x2A
+#define _hexadecimal_43 0x2B
+#define _hexadecimal_44 0x2C
+#define _hexadecimal_45 0x2D
+#define _hexadecimal_46 0x2E
+#define _hexadecimal_47 0x2F
+#define _hexadecimal_48 0x30
+#define _hexadecimal_49 0x31
+#define _hexadecimal_50 0x32
+#define _hexadecimal_51 0x33
+#define _hexadecimal_52 0x34
+#define _hexadecimal_53 0x35
+#define _hexadecimal_54 0x36
+#define _hexadecimal_55 0x37
+#define _hexadecimal_56 0x38
+#define _hexadecimal_57 0x39
+#define _hexadecimal_58 0x3A
+#define _hexadecimal_59 0x3B
+#define _hexadecimal_60 0x3C
+#define _hexadecimal_61 0x3D
+#define _hexadecimal_62 0x3E
+#define _hexadecimal_63 0x3F
+#define _hexadecimal_64 0x40
+
+#define __hexadecimal_(DEC) _hexadecimal_ ## DEC
+#define _hexadecimal_(DEC) __hexadecimal_(DEC)
+
 
 #endif 	    /* !ORWL_MACRO_H_ */
