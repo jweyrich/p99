770b105e69eacf64ad95d19f2c921002beaadee1
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-03-24 18:05:57 +0100

    implement a first version of a listening server thread
    untested

--- include/orwl_socket.h
+++ include/orwl_socket.h
@@ -11,9 +11,13 @@
 #ifndef   	ORWL_SOCKET_H_
 # define   	ORWL_SOCKET_H_
 
+#include <unistd.h>
+
 #include "orwl_macro.h"
 #include "orwl_int.h"
 #include "orwl_enum.h"
+#include "orwl_thread.h"
+#include "orwl_rand.h"
 
 inline
 void FUNC_DEFAULT(orwl_hton)(uint32_t *n, uint64_t const *h, size_t l) {
@@ -41,5 +45,84 @@ void FUNC_DEFAULT(orwl_ntoh)(uint64_t* h, uint32_t const *n, size_t l) {
 #define orwl_ntoh(...) DEFINE_FUNC_DEFAULT(orwl_ntoh, 3, __VA_ARGS__)
 declare_default_arg(orwl_ntoh, 2, size_t, 1);
 
+
+extern in_addr_t _inet4_addr;
+
+DECLARE_ONCE_UPON(inet4_addr, _inet4_addr);
+
+
+inline
+in_addr_t inet4_addr(void) {
+  INIT_ONCE_UPON(inet4_addr, _inet4_addr);
+  return _inet4_addr;
+}
+
+struct orwl_endpoint;
+
+#ifndef __cplusplus
+typedef struct orwl_endpoint orwl_endpoint;
+#endif
+
+struct orwl_endpoint {
+  uint64_t addr;
+  uint64_t port;
+};
+
+DOCUMENT_INIT(orwl_endpoint)
+FUNC_DEFAULT_DOCUMENTATION(orwl_endpoint_init)
+inline
+void FUNC_DEFAULT(orwl_endpoint_init)
+(orwl_endpoint *endpoint,
+ in_addr_t addr,
+ in_port_t port
+ ) {
+  orwl_endpoint const ep = { addr, port };
+  memcpy(endpoint, &ep, sizeof(orwl_endpoint));
+}
+
+DOCUMENT_DESTROY(orwl_endpoint)
+inline
+void orwl_endpoint_destroy(orwl_endpoint *endpoint) {
+  /* empty */
+}
+
+
+#define orwl_endpoint_init(...) DEFINE_FUNC_DEFAULT(orwl_endpoint_init, 3, __VA_ARGS__)
+
+declare_default_arg(orwl_endpoint_init, 2, in_port_t, TNULL(in_port_t));
+declare_default_arg(orwl_endpoint_init, 1, in_addr_t, TNULL(in_addr_t));
+
+DECLARE_NEW_DELETE(orwl_endpoint);
+
+void orwl_send(orwl_endpoint const* ep, rand48_t seed, uint64_t const* mess, size_t len);
+
+
+struct orwl_server;
+
+#ifndef __cplusplus
+typedef struct orwl_server orwl_server;
+#endif
+
+typedef void (*server_cb_t)(uint64_t const* mes, size_t n);
+
+struct orwl_server {
+  orwl_endpoint ep;
+  unsigned max_connections;
+  server_cb_t const cb;
+};
+
+
+void orwl_server_init(orwl_server *serv) {
+  memset(serv, 0, sizeof(orwl_server));
+}
+
+void orwl_server_destroy(orwl_server *serv) {
+  /* empty */
+}
+
+DECLARE_NEW_DELETE(orwl_server);
+DEFINE_NEW_DELETE(orwl_server);
+
+DECLARE_THREAD(orwl_server);
 
 #endif 	    /* !ORWL_SOCKET_H_ */
--- lib/orwl_socket.c
+++ lib/orwl_socket.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 
 #include "orwl_socket.h"
+#include "orwl_rand.h"
 
 static uint32_t mycode = 0;
 
@@ -28,8 +29,8 @@ do {                                                    \
  } while (false)
 
 
-DECLARE_ONCE(mycode);
-DEFINE_ONCE(mycode) {
+DECLARE_ONCE_UPON(mycode, mycode);
+DEFINE_ONCE_UPON(mycode, mycode) {
     ifCode(0, 1, 2, 3);
     ifCode(0, 1, 3, 2);
     ifCode(0, 2, 1, 3);
@@ -61,7 +62,7 @@ DEFINE_ONCE(mycode) {
 }
 
 bool same_endianess(uint32_t c) {
-  INIT_ONCE(mycode);
+  INIT_ONCE_UPON(mycode, mycode);
   return c == mycode;
 }
 
@@ -69,3 +70,204 @@ void FUNC_DEFAULT(orwl_hton)(uint32_t *n, uint64_t const *h, size_t l);
 define_default_arg(orwl_hton, 2, size_t);
 void FUNC_DEFAULT(orwl_ntoh)(uint64_t* h, uint32_t const *n, size_t l);
 define_default_arg(orwl_ntoh, 2, size_t);
+
+static
+bool orwl_send_(int fd, uint64_t const*mess, size_t len) {
+  uint32_t *buf = uint32_t_vnew(2 * len);
+  orwl_hton(buf, mess, len);
+  ssize_t ret = send(fd, &buf, sizeof(uint64_t) * len, MSG_WAITALL);
+  uint32_t_vdelete(buf);
+  return ret != sizeof(uint64_t) * len;
+}
+
+static
+bool orwl_recv_(int fd, uint64_t *mess, size_t len) {
+  uint32_t *buf = uint32_t_vnew(2 * len);
+  ssize_t ret = recv(fd, &buf, sizeof(uint64_t) * len, MSG_WAITALL);
+  orwl_ntoh(mess, buf , len);
+  uint32_t_vdelete(buf);
+  return ret != sizeof(uint64_t) * len;
+}
+
+
+in_addr_t _inet4_addr = INITIALIZER;
+
+DEFINE_ONCE_UPON(inet4_addr, _inet4_addr) {
+  char const* str = getenv("INET4");
+  struct in_addr inaddr = INITIALIZER;
+  if (inet_aton(str, &inaddr)) {
+    _inet4_addr = inaddr.s_addr;
+  }
+}
+
+in_addr_t inet4_addr(void);
+
+
+void FUNC_DEFAULT(orwl_endpoint_init)
+     (orwl_endpoint *endpoint,
+      in_addr_t addr,
+      in_port_t port
+      );
+
+void orwl_endpoint_destroy(orwl_endpoint *endpoint);
+
+
+define_default_arg(orwl_endpoint_init, 2, in_port_t);
+define_default_arg(orwl_endpoint_init, 1, in_addr_t);
+
+DEFINE_NEW_DELETE(orwl_endpoint);
+
+enum { header_t_els = 2 };
+
+typedef uint64_t header_t[header_t_els];
+
+struct auth_sock {
+  int fd;
+  size_t len;
+  server_cb_t const cb;
+};
+
+typedef struct auth_sock auth_sock;
+
+void auth_sock_init(auth_sock *sock) {
+  /* empty */
+}
+
+void auth_sock_destroy(auth_sock *sock) {
+  if (sock->fd != -1) close(sock->fd);
+  sock->fd = -1;
+}
+
+DECLARE_NEW_DELETE(auth_sock);
+DEFINE_NEW_DELETE(auth_sock);
+
+DECLARE_THREAD(auth_sock);
+
+
+DEFINE_THREAD(auth_sock) {
+  uint64_t *mess = uint64_t_vnew(Arg->len);
+  if (!orwl_recv_(Arg->fd, mess, Arg->len))
+    if (Arg->cb)
+      /* do something with mess here */
+      Arg->cb(mess, Arg->len);
+  /* now clean up */
+  uint64_t_vdelete(mess);
+  header_t header = INITIALIZER;
+  /* Ack the termination of the call */
+  orwl_send_(Arg->fd, header, header_t_els);
+  close(Arg->fd);
+}
+
+
+DEFINE_THREAD(orwl_server) {
+  int fd_listen = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd_listen != -1) {
+    rand48_t seed = { time(NULL) };
+    struct sockaddr_in addr = INITIALIZER;
+    socklen_t len = sizeof(addr);
+    addr.sin_addr.s_addr = Arg->ep.addr;
+    addr.sin_port = Arg->ep.port;
+    addr.sin_family = AF_INET;
+    if (bind(fd_listen, (struct sockaddr*) &addr, sizeof(addr)) == -1)
+      goto TERMINATE;
+    /* If the port was not yet specified find and store it. */
+    if (!addr.sin_port) {
+      if (getsockname(fd_listen, (struct sockaddr*)&addr, &len) == -1)
+        goto TERMINATE;
+      Arg->ep.port = addr.sin_port;
+    }
+    if (listen(fd_listen, Arg->max_connections) == -1)
+      goto TERMINATE;
+    while (fd_listen != -1) {
+      /* Do this work before being connected */
+      uint64_t chal = orwl_rand64(seed);
+      uint64_t repl = orwl_challenge(chal);
+      header_t header = INITIALIZER;
+
+      int fd = -1;
+      do {
+        fd = accept(fd_listen, NULL, &TNULL(socklen_t));
+      } while(fd == -1);
+
+      /* Receive a challenge from the new connection */
+      if (orwl_recv_(fd, header, header_t_els))
+        goto FINISH;
+      header[1] = orwl_challenge(header[0]);
+      header[0] = chal;
+      /* Send the reply and a new challenge to the new connection */
+      if (orwl_send_(fd, header, header_t_els))
+        goto FINISH;
+      /* Receive the reply and the message length from the new connection */
+      if (orwl_recv_(fd, header, header_t_els))
+        goto FINISH;
+      if (header[1] == repl) {
+        size_t len = header[0];
+        if (len) {
+          auth_sock sock = { .fd = fd, .len = len, .cb = Arg->cb };
+          auth_sock_create(&sock, NULL);
+          /* The spawned thread will close the fd. */
+          continue;
+        } else {
+          /* The authorized empty message indicates termination */
+          close(fd);
+          close(fd_listen);
+          fd_listen = -1;
+          break;
+        }
+      }
+    FINISH:
+      close(fd);
+    }
+  }
+ TERMINATE:
+  if (fd_listen != -1) close(fd_listen);
+}
+
+void orwl_send(orwl_endpoint const* ep, rand48_t seed, uint64_t const* mess, size_t len) {
+  /* do all this work before opening the socket */
+  uint64_t chal = orwl_rand64(seed);
+  uint64_t repl = orwl_challenge(chal);
+  header_t header = { [0] = chal };
+  struct sockaddr_in addr = {
+    .sin_addr = { .s_addr = ep->addr },
+    .sin_port = ep->port,
+    .sin_family = AF_INET
+  };
+
+  int fd = socket(PF_INET, SOCK_STREAM, 0);
+  if (fd == -1) return;
+
+  if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+    goto FINISH;
+  /* Send a challenge to the other side */
+  if (orwl_send_(fd, header, header_t_els))
+    goto FINISH;
+  /* Receive the answer and another challenge from the other side */
+  if (orwl_recv_(fd, header, header_t_els))
+    goto FINISH;
+  if (header[1] == repl) {
+    /* The other side is authorized. Send the answer and the size of
+       the message to the other side. */
+    header[1] = orwl_challenge(header[0]);
+    header[0] = len;
+    if (orwl_send_(fd, header, header_t_els))
+      goto FINISH;
+    /* The authorized empty message indicates termination.
+       If not so, we now send the real message. */
+    if (len) {
+      if (orwl_send_(fd, mess, len))
+        goto FINISH;
+      /* Receive a final message, until the other end closes the
+         connection. */
+      orwl_recv_(fd, header, header_t_els);
+    }
+  } else  {
+    /* The other side is not authorized. Terminate. */
+    header[1] = 0;
+    header[0] = 0;
+    if (orwl_send_(fd, header, header_t_els))
+      goto FINISH;
+  }
+ FINISH:
+  close(fd);
+}
