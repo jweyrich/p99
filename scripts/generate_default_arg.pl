7470c84f76da276617ce333a583865d541c01a95
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-04-04 19:26:22 +0200

    avoid one extra macro recursion

--- scripts/generate_default_arg.pl
+++ scripts/generate_default_arg.pl
@@ -37,8 +37,7 @@ for (my $i = 0; $i < $maxnumber; ++$i) {
 
 for (my $i = 1; $i < $maxnumber; ++$i) {
     my $i0 = $i - 1;
-    print "#define ___wda_${i}(NAME, N, ...) _wda_${i0}(NAME, __VA_ARGS__, NAME ## _defarg_ ## N())\n";
-    print "#define __wda_${i}(NAME, N, ...) ___wda_${i}(NAME, N, __VA_ARGS__)\n";
-    print "#define _wda_${i}(NAME, ...) __wda_${i}(NAME, _predecessor(_NARG_64(x, ## __VA_ARGS__)), __VA_ARGS__)\n";
+    print "#define __wda_${i}(NAME, N, ...) _wda_${i0}(NAME, __VA_ARGS__, PASTE3(NAME, _defarg_, N)())\n";
+    print "#define _wda_${i}(NAME, ...) __wda_${i}(NAME, _NARG_64(__VA_ARGS__), __VA_ARGS__)\n";
     print "#define _wda_minus_${i}(NAME, ...) __VA_ARGS__\n";
 }
