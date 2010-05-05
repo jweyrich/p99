2e209b6e6d5bcd1559a2e005f8b906d0d325cc65
Jens Gustedt <jens.gustedt@loria.fr>
2010-05-05 11:15:04 +0200

    older versions of gcc get really confused when a pointer to an inline function is needed

--- include/orwl_inline.h
+++ include/orwl_inline.h
@@ -9,7 +9,7 @@
 #   undef inline
 #  endif
 #  if (__GNUC__ < 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ < 3))
-#   define inline __attribute__((gnu_inline, always_inline)) extern __inline__
+#   define inline __attribute__((gnu_inline,always_inline)) static __inline__
 #  else
 #   define inline __inline__
 #  endif
