1dbb66033edf0fc1679bfdd1e8fb0498541942f1
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-05-09 00:52:46 +0200

    add a separate .h file for the header type shared between different .o files

---
+++ include/orwl_header.h
@@ -0,0 +1,21 @@
+/*
+** orwl_header.h
+** 
+** Made by Jens Gustedt
+** Login   <gustedt@damogran.loria.fr>
+** 
+** Started on  Fri May  7 21:33:11 2010 Jens Gustedt
+** Last update Fri May  7 21:33:11 2010 Jens Gustedt
+*/
+
+#ifndef   	ORWL_HEADER_H_
+# define   	ORWL_HEADER_H_
+
+#include "orwl_int.h"
+
+enum { header_t_els = 2 };
+
+typedef uint64_t header_t[header_t_els];
+
+
+#endif 	    /* !ORWL_HEADER_H_ */
