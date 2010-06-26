099cf08e758b22d9bbe5c4df216c5758033c1c51
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-06-26 21:01:41 +0200

    move all paste macros together

--- include/orwl_macro.h
+++ include/orwl_macro.h
@@ -12,56 +12,7 @@
 # define   	ORWL_MACRO_H_
 
 #include "orwl_inline.h"
-#include "orwl_default_arg.h"
-
-/* This was inspired by BOOT's PP_CAT macro. Using such a thing avoid
-   to define multiple levels of expansion for each macro. */
-#define PASTE0()
-#define PASTE1(_1) _1
-
-/**
- ** @brief Paste two token sequences at their junction.
- **
- ** This macro does the paste operation first, and then an evaluation
- ** of the result. Thus
- ** @code
- ** #define B(x) toto(x)
- ** CAT2(A, B(6));
- ** @endcode
- ** will result in
- ** @code
- ** AB(6);
- ** @endcode
- ** and not in
- ** @code
- ** Atoto(6)
- ** @endcode
- **/
-#define CAT2(_1, _2) _1 ## _2
-
-/**
- ** @brief Paste two token sequences at their junction.
- **
- ** This macro does the evaluation of the arguments first and then
- ** proceeds at the concatenation
- ** of the results. Thus
- ** @code
- ** #define B(x) toto(x)
- ** CAT2(A, B(6));
- ** @endcode
- ** will result in
- ** @code
- ** Atoto(6)
- ** @endcode
- ** and not in
- ** @code
- ** AB(6);
- ** @endcode
- **/
-#define PASTE2(_1, _2) CAT2(_1, _2)
-
-#define P99___PASTE(F, N, ...) F ## N(__VA_ARGS__)
-#define P99__PASTE(N, ...) P99___PASTE(PASTE, N, __VA_ARGS__)
+#include "p99_paste.h"
 
 #define P99__DEC_DOUBLE(SIGN, INT, FRAC, ESIGN, EXP, ...)   \
   IF_EMPTY(SIGN)(+)(SIGN)P99__SKIP_ PASTE(                  \
@@ -761,29 +712,6 @@ CHOOSE5(xT,                                     \
  ** @{
  **/
 
-
-/**
- ** @brief A left-to-right associative paste operator.
- **
- ** This macro avoids the ambiguity of the @c ## preprocessor operator
- ** which has no well defined associativity. With this macro here
- ** something like
- ** @code
- ** PASTE(0.1E, -, 1)
- ** @endcode
- ** is guaranteed to produce the token @c 0.1E-1, whereas the
- ** seemingly equivalent
- ** @code
- ** ETSAP(0.1E, -, 1)
- ** @endcode
- ** is not valid: the intermediate operation to paste tokens `-' and
- ** `1' would result in an invalid token and is thus rejected.
- **
- ** This macro does the evaluation of the arguments first and
- ** then proceeds at the concatenation of the results.
- ** @pre the argumentlist should not be empty.
- **/
-#define PASTE(...) P99__PASTE(P99__NARG(__VA_ARGS__), __VA_ARGS__)
 
 #define LAST(...) CHS(DEC_PRED(P99__NARG(__VA_ARGS__)), __VA_ARGS__,)
 #define ALLBUTLAST(...) PASTE2(P99__PRE,DEC_PRED(P99__NARG(__VA_ARGS__)))(__VA_ARGS__,)
