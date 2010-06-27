d22c9fdf97a6f3742ec9902227354fc49f24a0ee
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-06-27 13:38:32 +0200

    move the logical macros to their own file

--- include/orwl_macro.h
+++ include/orwl_macro.h
@@ -12,7 +12,7 @@
 # define   	ORWL_MACRO_H_
 
 #include "orwl_inline.h"
-#include "p99_args.h"
+#include "p99_logical.h"
 
 #define P99__DEC_DOUBLE(SIGN, INT, FRAC, ESIGN, EXP, ...)   \
   IF_EMPTY(SIGN)(+)(SIGN)P99__SKIP_ PASTE(                  \
@@ -151,8 +151,6 @@
  **/
 #define IF_DEC_GT(A, B) IF_LT_0(P99__dec_minus(B,A))
 
-#define IS_EQ(_0, _1) HAS_COMMA(PASTE4(P99__IS_, _0, _EQ_, _1))
-
 /**
  ** @brief Test if token N is the token 0.
  **/
@@ -181,78 +179,6 @@
 #define IF_EMPTY(...) IF_EQ_1(IS_EMPTY(__VA_ARGS__))
 
 /**
- ** @brief Do a evaluation of the argument.
- **
- ** The result is the token 0 (seen here as `false') if the argument
- ** was empty or if it evaluated to the token 0. Otherwise returns the
- ** token 1.
- **
- ** @return tokens 0 or 1
- **/
-#define LOGIC_EVAL(_0) P99__EVAL_0(IS_EMPTY(_0), IS_EQ_0(_0))
-
-#define P99__EVAL_0(_0, _1) PASTE3(P99__EVAL_, _0, _1)
-
-#define P99__EVAL_00 0
-#define P99__EVAL_10 1
-#define P99__EVAL_01 0
-/* should never happen */
-#define P99__EVAL_11 WEIRD_EVALUATION_ERROR
-
-/**
- ** @brief Do a logical negation of the argument.
- **
- ** The result is the token 1 (seen here as `true') if the argument
- ** was empty or if it evaluated to the token 0. Otherwise returns the
- ** token 0.
- **
- ** @return tokens 0 or 1
- **/
-#define LOGIC_NOT(A)  P99__NOT_0(IS_EMPTY(_0), IS_EQ_0(_0))
-
-#define P99__NOT_0(_0, _1) PASTE3(P99__NOT_, _0, _1)
-
-#define P99__NOT_00 1
-#define P99__NOT_10 0
-#define P99__NOT_01 1
-/* should never happen */
-#define P99__NOT_11 WEIRD_NEGATION_ERROR
-
-/**
- ** @brief Do a logical exclusive or of the arguments.
- **
- ** @see LOGIC_EVAL for how the individual arguments are considered to
- ** be truth values.
- **
- ** @return tokens 0 or 1
- **/
-//#define LOGIC_XOR(A, B) IF_EQ(LOGIC_NOT(A), LOGIC_EVAL(B))(1)(0)
-//#define P99__LOGIC_OR(A, B) IF_EQ(00, CAT2(A, B))(0)(1)
-
-/**
- ** @brief Do a logical inclusive or of the arguments.
- **
- ** @see LOGIC_EVAL for how the individual arguments are considered to
- ** be truth values.
- **
- ** @return tokens 0 or 1
- **/
-//#define LOGIC_OR(A, B) P99__LOGIC_OR(LOGIC_EVAL(A), LOGIC_EVAL(B))
-
-//#define P99__LOGIC_AND(A, B) IF_EQ(00, CAT2(A, B))(1)(0)
-
-/**
- ** @brief Do a logical and of the arguments.
- **
- ** @see LOGIC_EVAL for how the individual arguments are considered to
- ** be truth values.
- **
- ** @return tokens 0 or 1
- **/
-//#define LOGIC_AND(A, B) P99__LOGIC_AND(LOGIC_NOT(A), LOGIC_NOT(B))
-
-
-/**
  ** @brief A preprocessor control structure
  **
  ** Use this as follows
@@ -266,36 +192,7 @@
  **/
 #define IF_ELSE(...) IF_EQ_1(IS_EQ_0(__VA_ARGS__))
 
-#define P99__IS_void_EQ_void(...) ,
-
-/**
- ** @brief Test if the argument consists of exactly the word @c void.
- **
- ** @see IF_VOID for a macro that test whether or not its argument is
- ** empty @b or if it consists of the word @c void.
- **/
-#define IS_void(...)                                                    \
-IS_EQ_2(P99__NARG(P99__IS_void_EQ_ ## __VA_ARGS__ (~) P99__IS_void_EQ_ ## __VA_ARGS__))
-
 #define IF_void(...) IF_EQ_1(IS_void(__VA_ARGS__))
-
-
-/**
- ** @brief Test whether or not its argument is empty @b or if it
- ** consists of the word @c void.
- **
- ** @see IF_void for a macro that tests if the argument is exactly the
- ** word @c void.
- **/
-#define IS_VOID(...) P99__IS_VOID(IS_EMPTY(__VA_ARGS__), IS_void(__VA_ARGS__))
-
-#define P99__IS_VOID(_0, _1) PASTE3(P99__IS_VOID_, _0, _1)
-
-#define P99__IS_VOID_00 0
-#define P99__IS_VOID_01 1
-#define P99__IS_VOID_10 1
-/* should not happen */
-#define P99__IS_VOID_11 WEIRD_VOID_ARG_ERROR
 
 #define IF_VOID(...) IF_EQ_1(IS_VOID(__VA_ARGS__))
 
