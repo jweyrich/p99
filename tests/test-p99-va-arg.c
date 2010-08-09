bdcbd3670b3e88ba4823415f2969e78815d0e424
Jens Gustedt <Jens.Gustedt@loria.fr>
2010-08-09 22:06:58 +0200

    split the tests for the p99 package into several files

--- tests/test-p99-va-arg.c
+++ tests/test-p99-va-arg.c
@@ -18,37 +18,9 @@
 #include <stddef.h>
 
 #include "p99_compiler.h"
-#include "p99_int.h"
+#include "p99_args.h"
 
 #include <stdio.h>
-#include <signal.h>
-
-
-static
-char const* representation[4] = {
-  [p99_signed_representation_invalid]
-  = "it has an unknown representation for negative values that is not C99 compliant",
-  [p99_signed_representation_magnitude]
-  = "sign and magnitude",
-  [p99_signed_representation_ones]
-  = "one's complement",
-  [p99_signed_representation_twos]
-  = "two's complement",
-};
-
-#define SAYIT(T)                                                \
-printf("%20s:\t%20jd\t...\t%20ju,\t%5ssigned%20s\n",            \
-       #T,                                                      \
-       (intmax_t)P99_TMIN(T),                                   \
-       (uintmax_t)P99_TMAX(T),                                  \
-       (P99_ISSIGNED(T) ? "" : "un"),                           \
-       (!P99_ISSIGNED(T)                                        \
-        ? ""                                                    \
-        : representation[P99_SIGNED_REPRESENTATION(T)]))
-
-typedef enum { a1, b1 } enum1;
-typedef enum { a2 = -1, b2, c2 } enum2;
-
 
 unsigned P99_FSYMB(toto)(unsigned a, P99_VA_ARGS(number));
 #define toto(A, ...) P99_FSYMB(toto)(A, P99_LENGTH_VA_ARG(__VA_ARGS__))
@@ -116,36 +88,6 @@ intmax_t P99_FSYMB(p99_mins)(size_t number, intmax_t const*const arr) {
 }
 
 int main(int argc, char** argv) {
-  SAYIT(_Bool);
-  SAYIT(char);
-  SAYIT(unsigned char);
-  SAYIT(signed char);
-  SAYIT(unsigned short);
-  SAYIT(signed short);
-  SAYIT(unsigned);
-  SAYIT(signed);
-  SAYIT(unsigned long);
-  SAYIT(signed long);
-  SAYIT(unsigned long long);
-  SAYIT(signed long long);
-  SAYIT(uint8_t);
-  SAYIT(int8_t);
-  SAYIT(uint16_t);
-  SAYIT(int16_t);
-  SAYIT(uint32_t);
-  SAYIT(int32_t);
-  SAYIT(uint64_t);
-  SAYIT(int64_t);
-  SAYIT(wchar_t);
-  SAYIT(size_t);
-  SAYIT(sig_atomic_t);
-  SAYIT(ptrdiff_t);
-  SAYIT(uintptr_t);
-  SAYIT(intptr_t);
-  SAYIT(uintmax_t);
-  SAYIT(intmax_t);
-  SAYIT(enum1);
-  SAYIT(enum2);
   assert(toto(3, 1, 3, 5, 7) == 1u);
   assert(tutu(3, 1, 3, 5, 7) == 1u);
 }
