ead3222d80f7a8293f099f3215728348a8d286cd
Jens Gustedt <Jens.Gustedt@loria.fr>
2011-03-03 14:19:56 +0100

    continue the documentation of the matrix example

--- tests/test-p99-pow.c
+++ tests/test-p99-pow.c
@@ -119,21 +119,62 @@ transposeFunc(P99_AARG(double const, B, 2, nb), void* buf) {
 
 
 /* Use constants that represent the sizes of blocks for a
-   decomposition of the matrices. */
+   decomposition of the matrices used by the mult function.
+   The total amount of elements used in the inner part of the
+   iteration will be (istep + jstep)*kstep for A and B, and
+   istep*jstep for C. */
 enum {
   /* A block should be such that several such submatrices will
      reasonably fit into the cache on a decent machine. */
   blocksize = (1u << 21)/sizeof(double),
   /* istep * jstep should be such that the load into cache of any of
      the submatrices is associated to a large number of operations. */
-  istep = (1u << 4),
-  jstep = (1u << 4),
+  istep = (1u << 5),
+  jstep = (1u << 5),
   /* The k-dimension of the matrices is accessed sequentially, so
      this part should be longer to give the automatic prefetcher an
      occasion to kick in. */
-  kstep = blocksize/(istep*jstep)
+  kstep = (blocksize - istep*jstep)/(istep + jstep)
 };
 
+/* Matrix multiplication has three nested for loops. One for each
+   dimension and on for the dotproduct between the rows of A and the
+   columns of B. We split all these loops in two. One with a longer
+   step count, the values istep, jstep and kstep from above, and an
+   inner one that then goes through the individual elements.
+
+   For convenience we realize the inner three loops with step size 1
+   as a macro. This will help optimization where all the three
+   parameters ILEN, JLEN and KLEN correspond to compile time
+   constants, namely istep, jstep and kstep, respectively.
+
+   ILEN, JLEN and KLEN are the length of the steps in the 3
+   dimensions. Generally they will be equal to istep, jstep and kstep,
+   respectively, but will be less than that for the last iteration. */
+#define INNER_CASE(C, A, B, ISTART, ILEN, JSTART, JLEN, KSTART, KLEN)   \
+/* The local type that handles a partial line. Two of these will be     \
+   created below and then be used for the dot product. */               \
+typedef double const kLineFrag[KLEN];                                   \
+/* The local type for the partial line in the result matrix C. */       \
+typedef double cLine[JLEN];                                             \
+/* None of the loops are parallelized at this level. All threads        \
+   should have enough work to do such that they may reuse their         \
+   caches well. */                                                      \
+P99_DO(size_t, i0, ISTART, ILEN) {                                      \
+  register kLineFrag* AF = (kLineFrag*)&((*A)[i0][KSTART]);             \
+  register cLine*restrict CL = (cLine*)&(*C)[i0][JSTART];               \
+  P99_DO(size_t, j0, JSTART, JLEN) {                                    \
+    register kLineFrag* BF = (kLineFrag*)&((*B)[j0][KSTART]);           \
+    register double*restrict c = &(*CL)[j0-JSTART];                     \
+    *c = dotproduct(AF, BF, *c);                                        \
+  }                                                                     \
+}
+
+/* Compute the product of two 2D matrices. The (more or less) hidden
+   function. */
+/* The input matrices are called A and B, the length in the two
+   dimensions are called na0, na1, nb0 and nb1, respectively. */
+/* The matrix C receives the result. */
 inline
 void
 multFunc(P99_AARG(double, C, 2, nc),
@@ -150,52 +191,57 @@ multFunc(P99_AARG(double, C, 2, nc),
   zeroOut(C);
   double times1 = omp_get_wtime();
 
-  /* This loop cannot be parallelized because otherwise we would need
-     to reduce the entries of C */
+  /* The loop for the dot product cannot be easily parallelized
+     because it accumulates the result. If we would want to do this,
+     we would need to use reduction for the entries of C. */
   P99_DO(size_t, k, 0, na1, kstep) {
+    /* The loops in the 2 dimensions can be parallelized because every
+       entry of the result is computed independently. Organize a
+       sufficiently large block for every individual thread such that
+       it is able to reuse cached values repeatedly. For the general
+       execution a thread works on
+
+       (istep + jstep)*kstep
+
+       elements for matrices A and B, C must not necessarily be
+       cached. On these it performs istep*jstep*kstep operations, so
+
+       istep*jstep / (istep + jstep)
+
+       or about istep/2 operations per entry. */
     P99_PARALLEL_DO(size_t, i, 0, na0, istep) {
       P99_PARALLEL_DO(size_t, j, 0, nb1, jstep) {
-#define INNER_CASE(IINCR, JINCR, KINCR)                                 \
-        typedef double const kLineFrag[KINCR];                          \
-        typedef double cLine[JINCR];                                    \
-        P99_DO(size_t, i0, i, IINCR) {                                  \
-          register kLineFrag* AF = (kLineFrag*)&((*A)[i0][k]);          \
-          register cLine*restrict CL = (cLine*)&(*C)[i0][j];            \
-          P99_DO(size_t, j0, j, JINCR) {                                \
-            register kLineFrag* BF = (kLineFrag*)&((*BS)[j0][k]);       \
-            register double*restrict c = &(*CL)[j0-j];                  \
-            *c = dotproduct(AF, BF, *c);                                \
-          }                                                             \
-        }
         register size_t const kMax = (na1 - k);
         register size_t const iMax = (na0 - i);
         register size_t const jMax = (nb1 - j);
         if (P99_LIKELY(iMax >= istep)) {
           if (P99_LIKELY(jMax >= jstep)) {
             if (P99_LIKELY(kMax >= kstep)) {
-              INNER_CASE(istep, jstep, kstep);
+              /* This is the principal case for which we want to
+                 optimize as much as possible. */
+              INNER_CASE(C, A, BS, i, istep, j, jstep, k, kstep);
             } else {
-              INNER_CASE(istep, jstep, kMax);
+              INNER_CASE(C, A, BS, i, istep, j, jstep, k, kMax);
             }
           } else {
             if (kMax >= kstep) {
-              INNER_CASE(istep, jMax, kstep);
+              INNER_CASE(C, A, BS, i, istep, j, jMax, k, kstep);
             } else {
-              INNER_CASE(istep, jMax, kMax);
+              INNER_CASE(C, A, BS, i, istep, j, jMax, k, kMax);
             }
           }
         } else {
           if (jMax >= jstep) {
             if (kMax >= kstep) {
-              INNER_CASE(iMax, jstep, kstep);
+              INNER_CASE(C, A, BS, i, iMax, j, jstep, k, kstep);
             } else {
-              INNER_CASE(iMax, jstep, kMax);
+              INNER_CASE(C, A, BS, i, iMax, j, jstep, k, kMax);
             }
           } else {
             if (kMax >= kstep) {
-              INNER_CASE(iMax, jMax, kstep);
+              INNER_CASE(C, A, BS, i, iMax, j, jMax, k, kstep);
             } else {
-              INNER_CASE(iMax, jMax, kMax);
+              INNER_CASE(C, A, BS, i, iMax, j, jMax, k, kMax);
             }
           }
         }
@@ -263,6 +309,8 @@ int main(int argc, char*argv[]) {
   size_t n = strtoul(argv[1]);
   size_t m = strtoul(argv[2]);
   size_t k = strtoul(argv[3]);
+  printf("matrix length are n=%zu, m=%zu, k=%zu\n", n, m, k);
+  printf("step   length are n=%d, m=%d, k=%d\n", istep, jstep, kstep);
   P99_AREF(double, AR, n, k) = malloc(sizeof(*AR));
   P99_AREF(double, BR, k, m) = malloc(sizeof(*BR));
   /* for (size_t j = 0; j < n; ++j) */
