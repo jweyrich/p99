c6b1fc1fef92a463599b5ec23886f7ebbb8fb8de
Emmanuel Jeanvoine <emmanuel.jeanvoine@inria.fr>
2011-03-22 18:19:33 +0100

    Modified the graph read interface to handle DOT graph in input.

--- include/orwl_helpers.h
+++ include/orwl_helpers.h
@@ -7,110 +7,43 @@
 #include "orwl_remote_queue.h"
 #include "orwl_int.h"
 
-P99_DECLARE_STRUCT(orwl_locations);
-P99_DECLARE_STRUCT(orwl_neighbor);
 P99_DECLARE_STRUCT(orwl_vertex);
 P99_DECLARE_STRUCT(orwl_graph);
 P99_DECLARE_STRUCT(orwl_id);
 P99_DECLARE_STRUCT(orwl_address_book);
 
-struct orwl_locations {
-  size_t local;
-  size_t distant;
-};
-
-inline
-orwl_locations* orwl_locations_init(orwl_locations *locations,
-				    size_t local,
-				    size_t distant) {
-  if (locations) {
-    *locations = P99_LVAL(orwl_locations,
-			  .local = local,
-			  .distant = distant,
-			  );
-  }
-  return locations;
-}
-
-inline
-P99_PROTOTYPE(orwl_locations*, orwl_locations_init, orwl_locations*, size_t, size_t);
-#define orwl_locations_init(...) P99_CALL_DEFARG(orwl_locations_init, 3, __VA_ARGS__)
-P99_DECLARE_DEFARG(orwl_locations_init, , P99_0(size_t), P99_0(size_t));
-
-inline
-void orwl_locations_destroy(orwl_locations *locations) {
-}
-
-DECLARE_NEW_DELETE(orwl_locations);
-
-struct orwl_neighbor {
-  size_t id;
-  size_t nb_locations;
-  orwl_locations * locations;
-};
-
-inline
-orwl_neighbor* orwl_neighbor_init(orwl_neighbor *neighbor,
-				  size_t id,
-				  size_t nb_locations) {
-  if (neighbor) {
-    *neighbor = P99_LVAL(orwl_neighbor,
-			 .id = id,
-			 .nb_locations = nb_locations,
-			 .locations = NULL,
-			 );
-    if (nb_locations > 0)
-      neighbor->locations = orwl_locations_vnew(nb_locations);
-  }
-  return neighbor;
-}
-
-inline
-P99_PROTOTYPE(orwl_neighbor*, orwl_neighbor_init, orwl_neighbor*, size_t, size_t);
-#define orwl_neighbor_init(...) P99_CALL_DEFARG(orwl_neighbor_init, 3, __VA_ARGS__)
-P99_DECLARE_DEFARG(orwl_neighbor_init, , P99_0(size_t), P99_0(size_t));
-
-inline
-void orwl_neighbor_destroy(orwl_neighbor *neighbor) {
-  orwl_locations_vdelete(neighbor->locations);
-}
-
-DECLARE_NEW_DELETE(orwl_neighbor);
 
 struct orwl_vertex {
-  size_t id;
   size_t color;
   size_t nb_neighbors;
-  struct orwl_neighbor * neighbors;
+  char label[16];
+  size_t * neighbors;
 };
 
 inline
-orwl_vertex* orwl_vertex_init(orwl_vertex *vertex,
-			      size_t id,
-			      size_t color,
-			      size_t nb_neighbors) {
+orwl_vertex* orwl_vertex_init(orwl_vertex *vertex, size_t nb_neighbors) {
   if (vertex) {
     *vertex = P99_LVAL(orwl_vertex,
-		       .id = id,
-		       .color = color,
+		       .color = 0,
 		       .nb_neighbors = nb_neighbors,
 		       .neighbors = NULL,
+		       .label = {0},
 		       );
     if (nb_neighbors > 0)
-      vertex->neighbors = orwl_neighbor_vnew(nb_neighbors);
+      vertex->neighbors = size_t_vnew(nb_neighbors);
   }
  
   return vertex;
 }
 
 inline
-P99_PROTOTYPE(orwl_vertex*, orwl_vertex_init, orwl_vertex*, size_t, size_t, size_t);
-#define orwl_vertex_init(...) P99_CALL_DEFARG(orwl_vertex_init, 4, __VA_ARGS__)
-P99_DECLARE_DEFARG(orwl_vertex_init, , P99_0(size_t), P99_0(size_t), P99_0(size_t));
+P99_PROTOTYPE(orwl_vertex*, orwl_vertex_init, orwl_vertex*, size_t);
+#define orwl_vertex_init(...) P99_CALL_DEFARG(orwl_vertex_init, 2, __VA_ARGS__)
+P99_DECLARE_DEFARG(orwl_vertex_init, , P99_0(size_t));
 
 inline
 void orwl_vertex_destroy(orwl_vertex *vertex) {
-  orwl_neighbor_vdelete(vertex->neighbors);
+  size_t_vdelete(vertex->neighbors);
 }
 
 DECLARE_NEW_DELETE(orwl_vertex);
@@ -139,7 +72,7 @@ void orwl_graph_destroy(orwl_graph *graph) {
 
 DECLARE_NEW_DELETE(orwl_graph);
 
-orwl_graph * orwl_graph_read(orwl_graph * graph, char const* file);
+orwl_graph * orwl_graph_read(orwl_graph * graph, char const* file, size_t nb_vertices);
 
 struct orwl_id {
   size_t num_id;
@@ -201,33 +134,38 @@ DECLARE_NEW_DELETE(orwl_address_book);
 
 orwl_address_book* orwl_address_book_read(orwl_address_book *ab, char const *file);
 
-bool orwl_wait_and_load_init_files(orwl_address_book *ab,
+bool orwl_wait_and_load_init_files(orwl_address_book **ab,
 				   const char *ab_filename,
-				   orwl_graph * graph,
+				   orwl_graph **graph,
 				   const char *graph_filename,
 				   orwl_server *serv,
 				   const char *id_filename,
 				   size_t nb_id,
 				   size_t *list_id,
-				   const char *lock_filename);
+				   size_t nb_vertices);
 
-void orwl_make_connections(size_t id, 
-			   orwl_server *server,
-			   orwl_graph *graph, 
-			   orwl_address_book *ab,
-			   orwl_mirror* locations);
+void orwl_make_connection(size_t dest_id,
+			  orwl_server *server,
+			  orwl_graph *graph, 
+			  orwl_address_book *ab,
+			  orwl_mirror *location,
+			  size_t dest_location_pos);
 
-void orwl_wait_to_initialize_locks(size_t id,
+bool orwl_wait_to_initialize_locks(size_t id,
 				   orwl_graph *graph,
+				   orwl_address_book *ab,
 				   orwl_server *server,
 				   orwl_mirror *my_locations,
 				   orwl_handle *h,
-				   size_t nb_locations);
+				   size_t nb_locations,
+				   rand48_t *seed);
 
-void orwl_wait_to_start(size_t id,
+bool orwl_wait_to_start(size_t id,
 			orwl_graph *graph,
+			orwl_address_book *ab,
 			orwl_server *server,
 			orwl_handle *h,
-			size_t nb_locations);
+			size_t nb_locations,
+			rand48_t *seed);
 
 #endif
--- lib/orwl_helpers.c
+++ lib/orwl_helpers.c
@@ -1,28 +1,20 @@
 #include "orwl_helpers.h"
 #include "p99_c99_default.h"
 #include "p99_str.h"
+#include "orwl_auth_sock_functions.h"
+#include "limits.h"
 
-#define MAX_LINE_SIZE 2048
-
-DEFINE_NEW_DELETE(orwl_locations);
-DEFINE_NEW_DELETE(orwl_neighbor);
 DEFINE_NEW_DELETE(orwl_vertex);
 DEFINE_NEW_DELETE(orwl_graph);
 DEFINE_NEW_DELETE(orwl_id);
 DEFINE_NEW_DELETE(orwl_address_book);
 
-P99_DEFINE_DEFARG(orwl_locations_init, , P99_0(size_t), P99_0(size_t));
-P99_DEFINE_DEFARG(orwl_neighbor_init, , P99_0(size_t), P99_0(size_t));
-P99_DEFINE_DEFARG(orwl_vertex_init, , P99_0(size_t), P99_0(size_t), P99_0(size_t));
+P99_DEFINE_DEFARG(orwl_vertex_init, , P99_0(size_t));
 P99_DEFINE_DEFARG(orwl_graph_init, , P99_0(size_t));
 P99_DEFINE_DEFARG(orwl_id_init, , P99_0(size_t), P99_0(char const*));
 P99_DEFINE_DEFARG(orwl_address_book_init, , P99_0(size_t));
 
-P99_INSTANTIATE(orwl_locations*, orwl_locations_init, orwl_locations*, size_t, size_t);
-P99_INSTANTIATE(void, orwl_locations_destroy, orwl_locations*);
-P99_INSTANTIATE(orwl_neighbor*, orwl_neighbor_init, orwl_neighbor*, size_t, size_t);
-P99_INSTANTIATE(void, orwl_neighbor_destroy, orwl_neighbor*);
-P99_INSTANTIATE(orwl_vertex*, orwl_vertex_init, orwl_vertex*, size_t, size_t, size_t);
+P99_INSTANTIATE(orwl_vertex*, orwl_vertex_init, orwl_vertex*, size_t);
 P99_INSTANTIATE(void, orwl_vertex_destroy, orwl_vertex*);
 P99_INSTANTIATE(orwl_graph*, orwl_graph_init, orwl_graph *, size_t);
 P99_INSTANTIATE(void, orwl_graph_destroy, orwl_graph *);
@@ -30,7 +22,6 @@ P99_INSTANTIATE(orwl_id*, orwl_id_init, orwl_id*, size_t, char const*);
 P99_INSTANTIATE(void, orwl_id_destroy, orwl_id*);
 P99_INSTANTIATE(orwl_address_book*, orwl_address_book_init, orwl_address_book *, size_t);
 P99_INSTANTIATE(void, orwl_address_book_destroy, orwl_address_book *);
-
 
 unsigned strcountchr(char *str, char chr) {
   unsigned count = 0;
@@ -45,7 +36,7 @@ unsigned strcountchr(char *str, char chr) {
   return count;
 }
 
-unsigned strsplit(char const *str, char str_array[][MAX_LINE_SIZE], char delim) {
+unsigned strsplit(char const *str, char str_array[][LINE_MAX], char delim) {
   unsigned count = 0;
   char *token;
   char *tmp = strdup(str);
@@ -53,7 +44,7 @@ unsigned strsplit(char const *str, char str_array[][MAX_LINE_SIZE], char delim) 
   if (strcountchr(tmp, delim) > 0) {
     token = strtok_r(tmp, &delim, &saveptr);
     while (token != NULL) {
-      strncpy(str_array[count], token, MAX_LINE_SIZE);
+      strncpy(str_array[count], token, LINE_MAX);
       token = strtok_r(NULL, &delim, &saveptr);
       count++;
     }
@@ -73,91 +64,83 @@ unsigned pow2(unsigned exp) {
   }
 }
 
-orwl_neighbor * orwl_graph_find_neighbor(orwl_vertex *vertex, size_t id) {
+bool orwl_graph_find_neighbor(orwl_vertex *vertex, size_t id) {
   if (vertex != NULL)
     for (size_t i = 0 ; i < vertex->nb_neighbors ; i++)
-      if (vertex->neighbors[i].id == id) return &vertex->neighbors[i];
-  return NULL;
+      if (vertex->neighbors[i] == id) return true;
+  return false;
 }
 
-orwl_neighbor * orwl_graph_insert_neighbor(orwl_vertex * vertex,
-					   size_t id,
-					   size_t nb_locations) {
-  if ((vertex != NULL) && orwl_graph_find_neighbor(vertex, id) == NULL) {
-    size_t i = 0;
-    while (vertex->neighbors[i].id != 0) i++;
-    orwl_neighbor_init(&vertex->neighbors[i], id, nb_locations);
-    return &vertex->neighbors[i];
-  }
-  return NULL;
+size_t extract_size_t_from_str(int start, int end, char const *original_str) {
+  const size_t size = end - start;
+  char sub_str[size];
+  strncpy(sub_str, &original_str[start], size);
+  sub_str[size] = '\0';
+  return strtouz(sub_str);
 }
 
-orwl_vertex * orwl_graph_find_vertex(orwl_graph * graph, size_t id) {
-  if (graph != NULL)
-    for (size_t i = 0 ; i < graph->nb_vertices ; i++)
-      if (graph->vertices[i].id == id) return &graph->vertices[i];
-  return NULL;
+void extract_str_from_str(int start, int end, char const *original_str, char *dest_str) {
+  const size_t size = end - start;
+  strncpy(dest_str, &original_str[start], size);
+  dest_str[size] = '\0';
 }
 
-orwl_vertex * orwl_graph_insert_vertex(orwl_graph * graph,
-				       size_t id,
-				       size_t color,
-				       size_t nb_neighbors) {
-  if ((graph != NULL) && orwl_graph_find_vertex(graph, id) == NULL) {
-    size_t i = 0;
-    while (graph->vertices[i].id != 0) i++;
-    orwl_vertex_init(&graph->vertices[i], id, color, nb_neighbors);
-    return &graph->vertices[i];
-  }
-  return NULL;
-}
+bool orwl_graph_extract_line(orwl_graph *graph,
+			     char const *str, int pass,
+			     size_t *nb_neighbors,
+			     regex_t *re_connection,
+			     regex_t *re_attributes) {
 
-bool orwl_graph_extract_line(orwl_graph * graph, char const * str) {
-  /*
-    A line pattern is as follow:
-    vertex_nb,vertex_color:neighbor-local_location#distant_location(,local_location#distant_location)+(;neighbor-local_location#distant_location(,local_location#distant_location)+)+
-   */
-  char split_l1[2][MAX_LINE_SIZE] = {0};
-  if (strsplit(str, split_l1, ':') != 2)
-    return false;
-  char split_l11[2][MAX_LINE_SIZE] = {0};
-  if (strsplit(split_l1[0], split_l11, ';') != 2)
-    return false;
-  size_t id = strtouz(split_l11[0]);
-  if (id < 1)
-    return false;
-  size_t color = strtouz(split_l11[1]);
-  const size_t neighbors = strcountchr(split_l1[1], ';') + 1;
-  orwl_vertex * pvertex = orwl_graph_insert_vertex(graph, id, color, neighbors);
-  if (pvertex == NULL) 
-    return false;
-  char split_l12[neighbors][MAX_LINE_SIZE];
-  if (strsplit(split_l1[1], split_l12, ';') != neighbors)
-    return false;
-  for (size_t i = 0 ; i < neighbors ; i++) {
-    char split_l121[2][MAX_LINE_SIZE] = {0};
-    if (strsplit(split_l12[i], split_l121, '-') != 2)
-      return false;
-    size_t id_neighbor = strtouz(split_l121[0]);
-    const size_t nb_locations = strcountchr(split_l121[1], ',') + 1;
-    orwl_neighbor * pneighbor = orwl_graph_insert_neighbor(pvertex, id_neighbor, nb_locations);
-    if (pneighbor == NULL)
-      return NULL;
-    char split_l122[nb_locations][MAX_LINE_SIZE];
-    if (strsplit(split_l121[1], split_l122, ',') != nb_locations)
-      return false;
-    for (size_t j = 0 ; j < nb_locations ; j++) {
-      char split_l1221[2][MAX_LINE_SIZE];
-      if (strsplit(split_l122[j], split_l1221, '#') != 2)
-	return false;
-      pneighbor->locations[j].local = strtouz(split_l1221[0]);
-      pneighbor->locations[j].distant = strtouz(split_l1221[1]);
+  regmatch_t connection_match[3]= {0};
+  regmatch_t attributes_match[4]= {0};
+  
+  /* matching a connection line */
+  if (regexec(re_connection, str, 3, connection_match, 0) == 0) {
+    size_t src_vertex = extract_size_t_from_str(connection_match[1].rm_so,
+						connection_match[1].rm_eo,
+						str);
+    size_t dst_vertex = extract_size_t_from_str(connection_match[2].rm_so,
+						connection_match[2].rm_eo,
+						str);
+    if (pass == 0) {
+      if (src_vertex == 1) {
+	printf("reading 1 - %zu:%zu - %s\n",src_vertex, dst_vertex, str);
+      }
+      nb_neighbors[src_vertex]++;
+    } else {
+      if (graph->vertices[src_vertex].neighbors == NULL) {
+	size_t nb = nb_neighbors[src_vertex];
+	orwl_vertex_init(&graph->vertices[src_vertex], nb);
+	for (size_t i = 0 ; i < nb ; i++)
+	  graph->vertices[src_vertex].neighbors[i] = SIZE_MAX;
+      }
+      size_t i = 0;
+      while (graph->vertices[src_vertex].neighbors[i] != SIZE_MAX) i++;
+      graph->vertices[src_vertex].neighbors[i] = dst_vertex;
     }
+  }
+  /* matching an attribute line (only at the second pass) */
+  if ((pass == 1) && (regexec(re_attributes, str, 4, attributes_match, 0) == 0)) {
+    size_t vertex = extract_size_t_from_str(attributes_match[1].rm_so,
+					    attributes_match[1].rm_eo,
+					    str);
+    
+    size_t color = extract_size_t_from_str(attributes_match[2].rm_so,
+					   attributes_match[2].rm_eo,
+					   str);
+    
+    char label[16] = {0};
+    extract_str_from_str(attributes_match[3].rm_so,
+			 attributes_match[3].rm_eo,
+			 str,
+			 &label[0]);
+    graph->vertices[vertex].color = color;
+    strncpy(graph->vertices[vertex].label, label, 16);
   }
   return true;
 }
 
-orwl_graph * orwl_graph_read(orwl_graph * graph, char const* file) {
+orwl_graph * orwl_graph_read(orwl_graph * graph, char const* file, size_t nb_vertices) {
   FILE*volatile f = fopen(file, "r");
   if (f == NULL) {
     P99_HANDLE_ERRNO {
@@ -167,38 +150,63 @@ orwl_graph * orwl_graph_read(orwl_graph * graph, char const* file) {
       }
     }
   }
-  P99_UNWIND_PROTECT {
-    char msg[6] = {0};
-    if (!fgets(msg, 6, f)) {
+  graph = P99_NEW(orwl_graph, nb_vertices);
+  char msg[LINE_MAX] = {0};
+  size_t nb_neighbors[nb_vertices];
+  for (size_t i = 0 ; i < nb_vertices ; i++)
+    nb_neighbors[i] = 0;
+
+  const char *connection = "^[[:blank:]]*([[:digit:]]+)[[:blank:]]*->[[:blank:]]*([[:digit:]]+)[[:blank:]]*";
+  const char *attributes = "^[[:blank:]]*([[:digit:]]+) \\[color=\"([[:digit:]]+)\",[[:blank:]]*label=\"([[:alnum:]]+)\"\\]";
+  regex_t re_connection, re_attributes;
+
+  if (regcomp(&re_connection, connection, REG_EXTENDED)) {
+    printf("Connection pattern did not compile.\n");
+    return NULL;
+  }
+  if (regcomp(&re_attributes, attributes, REG_EXTENDED)) {
+    printf("Attributes pattern did not compile.\n");
+    return NULL;
+  }
+
+  for (size_t pass = 0 ; pass < 2 ; pass++) {
+    if (pass == 1) {
+      for (size_t i = 0 ; i < graph->nb_vertices ; i++) {
+	printf("VOISINS %zu -> %zu\n", i,  nb_neighbors[i]);
+      }
+    }
+    fseek(f, 0, SEEK_SET);
+    P99_HANDLE_ERRNO {
+    P99_XDEFAULT : {
+	perror("error when seeking the begin of the file");
+	fclose(f);
+	return NULL;
+      }
+    }
+    while (fgets(msg, LINE_MAX, f) != NULL) {
       P99_HANDLE_ERRNO {
       P99_XDEFAULT : {
-	  perror("error when reading the number of vertices in graph file");
-	  P99_UNWIND_RETURN NULL;
+	  perror("error when reading a line in graph file");
+	  orwl_graph_delete(graph);
+	  fclose(f);
+	  regfree(&re_connection);
+	  regfree(&re_attributes);
+	  return NULL;
 	}
       }
-    }
-    size_t nb_vertices = str2uz(msg);
-    graph = P99_NEW(orwl_graph, nb_vertices);
-    for (size_t i = 0; i < nb_vertices; i++) {
-      char msg[MAX_LINE_SIZE] = {0};
-      if (!fgets(msg, MAX_LINE_SIZE, f)) {
-	P99_HANDLE_ERRNO {
-	P99_XDEFAULT : {
-	    perror("error when reading the number of vertices in graph file");
-	    orwl_graph_delete(graph);
-	    P99_UNWIND_RETURN NULL;
-	  }
-	}
-      }
-      if (!orwl_graph_extract_line(graph, msg)) {
+      if (!orwl_graph_extract_line(graph, msg, pass, nb_neighbors, &re_connection, &re_attributes)) {
 	printf("error while extracting a line in the graph file\n");
 	orwl_graph_delete(graph);
-	P99_UNWIND_RETURN NULL;
+	fclose(f);
+	regfree(&re_connection);
+	regfree(&re_attributes);
+	return NULL;
       }
     }
-  P99_PROTECT:
-    fclose(f);
   }
+
+  regfree(&re_connection);
+  regfree(&re_attributes);
   return graph;
 }
 
@@ -226,7 +234,7 @@ bool orwl_address_book_extract_line(orwl_address_book *ab, char const *str) {
     A line pattern is as follow:
     id-host
    */
-  char split_l[2][MAX_LINE_SIZE] = {0};
+  char split_l[2][LINE_MAX] = {0};
   if (strsplit(str, split_l, '-') != 2)
     return false;
   size_t id = strtouz(split_l[0]);
@@ -260,8 +268,8 @@ orwl_address_book* orwl_address_book_read(orwl_address_book *ab, char const *fil
     size_t nb_ids = str2uz(msg);
     ab = P99_NEW(orwl_address_book, nb_ids);
     for (size_t i = 0; i < nb_ids; i++) {
-      char msg[MAX_LINE_SIZE] = {0};
-      if (!fgets(msg, MAX_LINE_SIZE, f)) {
+      char msg[LINE_MAX] = {0};
+      if (!fgets(msg, LINE_MAX, f)) {
 	P99_HANDLE_ERRNO {
 	P99_XDEFAULT : {
 	    perror("error when reading the number of ids in address book file");
@@ -308,18 +316,6 @@ bool orwl_dump_id(orwl_server *serv,
   return true;
 }
 
-bool orwl_write_lockfile(const char *filename) {
-  FILE * f = NULL;
-  
-  f = fopen(filename, "w");
-  if (f == NULL) {
-    perror("error when writing the lock file");
-    return false;
-  }
-  fclose(f);
-  return true;
-}
-
 void orwl_wait_until_file_is_here(const char *filename) {
   FILE * f = NULL;
   
@@ -329,145 +325,155 @@ void orwl_wait_until_file_is_here(const char *filename) {
   }
 }
 
-bool orwl_wait_and_load_init_files(orwl_address_book *ab,
+bool orwl_wait_and_load_init_files(orwl_address_book **ab,
 				   const char *ab_filename,
-				   orwl_graph * graph,
+				   orwl_graph **graph,
 				   const char *graph_filename,
 				   orwl_server *serv,
 				   const char *id_filename,
 				   size_t nb_id,
 				   size_t *list_id,
-				   const char *lock_filename) {
+				   size_t nb_vertices) {
   if (!orwl_dump_id(serv, id_filename, nb_id, list_id))
     return false;
-  if (!orwl_write_lockfile(lock_filename))
+  orwl_wait_until_file_is_here(id_filename);
+  *ab = orwl_address_book_read(*ab, ab_filename);
+  if (*ab == NULL)
     return false;
-  orwl_wait_until_file_is_here(lock_filename);
-  ab = orwl_address_book_read(ab, ab_filename);
-  if (ab == NULL)
-    return false;
-  graph = orwl_graph_read(graph, graph_filename);
-  if (graph == NULL)
+  *graph = orwl_graph_read(*graph, graph_filename, nb_vertices);
+  if (*graph == NULL)
     return false;
   return true;
 }
 
-void orwl_make_connections(size_t id, 
-			   orwl_server *server,
-			   orwl_graph *graph, 
-			   orwl_address_book *ab,
-			   orwl_mirror* locations) {
-  orwl_vertex * me = orwl_graph_find_vertex(graph, id);
-  for (size_t i = 0 ; i < me->nb_neighbors ; i++) {
-    orwl_id * neighbor_id = orwl_address_book_find_id(ab, me->neighbors[i].id);
-    for (size_t j = 0 ; j < me->neighbors[i].nb_locations ; j++) {      
-      orwl_endpoint there = neighbor_id->ep;
-      there.index = me->neighbors[i].locations[j].distant;
-      orwl_mirror_connect(&locations[me->neighbors[i].locations[j].local], server, there);
-    }
-  }
+void orwl_make_connection(size_t dest_id,
+			  orwl_server *server,
+			  orwl_graph *graph, 
+			  orwl_address_book *ab,
+			  orwl_mirror *location,
+			  size_t dest_location_pos) {
+  orwl_id * dest = orwl_address_book_find_id(ab, dest_id);
+  orwl_endpoint there = dest->ep;
+  there.index = dest_location_pos;
+  orwl_mirror_connect(location, server, there);
 }
 
 
-bool rpc_check_colored_init_finished(void) {
-  /* TODO something here */
-
-  return true;
+bool rpc_check_colored_init_finished(size_t id,
+				     orwl_graph *graph,
+				     orwl_address_book *ab,
+				     rand48_t *seed) {
+  orwl_id * dest_id = orwl_address_book_find_id(ab, id);
+  orwl_endpoint there = dest_id->ep;
+  /* warning, this is a blocking operation */
+  return (orwl_rpc(&there, seed, auth_sock_check_initialization, pow2(id)) == 1);
 }
-
 
 size_t orwl_get_neighbors_in_undirected_graph(orwl_vertex **my_neighbors,
-					      orwl_vertex * me,
+					      size_t id,
 					      size_t max_neighbors,
 					      orwl_graph *graph) {
   size_t current = 0;
+  orwl_vertex * me = &graph->vertices[id];
   /* first we add the neighbors where I request locks */
   for (size_t i = 0 ; i < me->nb_neighbors ; i++)
-    my_neighbors[current++] = orwl_graph_find_vertex(graph, me->neighbors[i].id);
+    my_neighbors[current++] = &graph->vertices[me->neighbors[i]];
   /* then we add the neighbors that request my locks */
   for (size_t i = 0 ; i < graph->nb_vertices ; i++) {
-    if (orwl_graph_find_neighbor(&graph->vertices[i], me->id) != NULL) {
-      bool found = false;
-      for (size_t j = 0 ; j < current ; j++) {
-	if (&graph->vertices[i] == my_neighbors[j]) {
-	  found = true;
-	  break;
+    if (i != id) 
+      if (orwl_graph_find_neighbor(&graph->vertices[i], id)) {
+	bool found = false;
+	for (size_t j = 0 ; j < current ; j++) {
+	  if (&graph->vertices[i] == my_neighbors[j]) {
+	    found = true;
+	    break;
+	  }
 	}
+	if (!found)
+	  my_neighbors[current++] = &graph->vertices[i];
       }
-      if (!found) {
-	my_neighbors[current++] = &graph->vertices[i];
-      }
-    }
   }
   return current;
 }
 
-void orwl_wait_to_initialize_locks(size_t id,
+size_t orwl_get_id_from_vertex(orwl_graph *graph, orwl_vertex *vertex) {
+  for (size_t i = 0 ; i < graph->nb_vertices ; i++)
+    if (&graph->vertices[i] == vertex)
+      return i;
+  return SIZE_MAX;
+}
+
+bool orwl_wait_to_initialize_locks(size_t id,
 				   orwl_graph *graph,
+				   orwl_address_book *ab,
 				   orwl_server *server,
 				   orwl_mirror *my_locations,
 				   orwl_handle *h,
-				   size_t nb_locations) {
-  orwl_vertex * me = orwl_graph_find_vertex(graph, id);
-  orwl_vertex * my_neighbors[graph->nb_vertices];
+				   size_t nb_locations,
+				   rand48_t *seed) {
+  orwl_vertex * me = &graph->vertices[id];
+  orwl_vertex * my_neighbors[graph->nb_vertices]; /* upper limit */
   for (size_t i = 0 ; i < graph->nb_vertices ; i++)
     my_neighbors[i] = NULL;
 
   size_t nb = orwl_get_neighbors_in_undirected_graph(my_neighbors,
-						     me,
-						     graph->nb_vertices,
-						     graph);
-  bool should_wait;
-  do { 
-    should_wait = false;
-    for (size_t i = 0 ; i < nb ; i++) {
-      if (my_neighbors[i]->color < me->color) {
-	//is it already finished ? if yes, we can go
-	if (!rpc_check_colored_init_finished()) {
-	  should_wait = true;
-	  sleepfor(0.5);
-	  break;
-	}
-      }
-    }
-  } while (should_wait);
+  						     id,
+  						     graph->nb_vertices,
+  						     graph);
 
+  for (size_t i = 0 ; i < nb ; i++) {
+    /* we ensure that the neighbors with a lower color are initialized */
+    if (my_neighbors[i]->color < me->color) {
+      size_t vertex_id = orwl_get_id_from_vertex(graph, my_neighbors[i]);
+      if (vertex_id == SIZE_MAX) {
+	printf("error when getting the id number of a vertex");
+	return false;
+      }
+      rpc_check_colored_init_finished(vertex_id, graph, ab, seed);
+    }
+  }
+  
+  /* locking all the locations */
   for (size_t i = 0 ; i < nb_locations ; i++) {
     orwl_write_request(my_locations, &h[i]);
     orwl_acquire(&h[i]);
   }
+  return true;
+}
+
+bool orwl_wait_to_start(size_t id,
+			orwl_graph *graph,
+			orwl_address_book *ab,
+			orwl_server *server,
+			orwl_handle *h,
+			size_t nb_locations,
+			rand48_t *seed) {
 
   ORWL_CRITICAL {
     server->id_initialized = server->id_initialized | pow2(id);
   }
-}
 
-void orwl_wait_to_start(size_t id,
-			orwl_graph *graph,
-			orwl_server *server,
-			orwl_handle *h,
-			size_t nb_locations) {
-  orwl_vertex * me = orwl_graph_find_vertex(graph, id);
   orwl_vertex * my_neighbors[graph->nb_vertices];
   for (size_t i = 0 ; i < graph->nb_vertices ; i++)
     my_neighbors[i] = NULL;
   
   size_t nb = orwl_get_neighbors_in_undirected_graph(my_neighbors,
-						     me,
-						     graph->nb_vertices,
-						     graph);
-  bool should_wait;
-  do {
-    should_wait = false;
-    for (size_t i = 0 ; i < nb ; i++) {
-      if (!rpc_check_colored_init_finished()) {
-	should_wait = true;
-	sleepfor(0.5);
-	break;
+  						     id,
+  						     graph->nb_vertices,
+  						     graph);
+  /* we ensure that all the neighbors are initialized */
+  for (size_t i = 0 ; i < nb ; i++) {
+    size_t vertex_id = orwl_get_id_from_vertex(graph, my_neighbors[i]);
+      if (vertex_id == SIZE_MAX) {
+	printf("error when getting the id number of a vertex");
+	return false;
       }
-    }
-  } while (should_wait);
+      rpc_check_colored_init_finished(vertex_id, graph, ab, seed);
+  }
 
+  /* unlocking the locations */
   for (size_t i = 0 ; i < nb_locations ; i++)
     orwl_release(&h[i]);
+  
+  return true;
 }
