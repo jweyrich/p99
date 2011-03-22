b0cf78264ec6e400a7d49f5b33ce8c484a226daf
(no author) <(no author)@no.where>
2011-03-22 10:52:49 +0100

    update copyrights
    Signed-off-by: Jens Gustedt <Jens.Gustedt@loria.fr>

--- lib/orwl_helpers.c
+++ lib/orwl_helpers.c
@@ -1,3 +1,17 @@
+/* This may look like nonsense, but it really is -*- mode: C -*-             */
+/*                                                                           */
+/* Except of parts copied from previous work and as explicitly stated below, */
+/* the authors and copyright holders for this work are as follows:           */
+/* all rights reserved,  2011 Emmanuel Jeanvoine, INRIA, France              */
+/* all rights reserved,  2011 Jens Gustedt, INRIA, France                    */
+/*                                                                           */
+/* This file is part of the P99 project. You received this file as as        */
+/* part of a confidential agreement and you may generally not                */
+/* redistribute it and/or modify it, unless under the terms as given in      */
+/* the file LICENSE.  It is distributed without any warranty; without        */
+/* even the implied warranty of merchantability or fitness for a             */
+/* particular purpose.                                                       */
+/*                                                                           */
 #include "orwl_helpers.h"
 #include "p99_c99_default.h"
 #include "p99_str.h"
