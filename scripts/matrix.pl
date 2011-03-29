1580ecba9660ee2d4f4c0d8d7cfdfa890beaaadd
(no author) <(no author)@no.where>
2011-03-29 10:38:39 +0200

    add copyright
    Signed-off-by: Jens Gustedt <Jens.Gustedt@loria.fr>

--- scripts/matrix.pl
+++ scripts/matrix.pl
@@ -1,3 +1,22 @@
+#!/bin/sh -f
+#  This may look like complete nonsense, but it really is -*- mode: perl; coding: utf-8 -*-
+eval 'exec perl -wS -x $0 ${1+"$@"}'
+if 0;               #### for this magic, see findSvnAuthors ####
+#!perl
+#
+#
+# Except of parts copied from previous work and as explicitly stated below,
+# the authors and copyright holders for this work are as follows:
+# all rights reserved,  2011 Matias E. Vara, INRIA, France
+#
+# This file is part of the P99 project. You received this file as as
+# part of a confidential agreement and you may generally not
+# redistribute it and/or modify it, unless under the terms as given in
+# the file LICENSE.  It is distributed without any warranty; without
+# even the implied warranty of merchantability or fitness for a
+# particular purpose.
+#
+
 #
 # matrix.pl
 #
