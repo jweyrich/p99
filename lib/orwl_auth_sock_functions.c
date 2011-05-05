07a8fac32d5f284e5b49da5379171ba4f9548c17
Jens Gustedt <Jens.Gustedt@loria.fr>
2011-05-05 10:08:24 +0200

    let the initial connection be less verbose

--- lib/orwl_auth_sock_functions.c
+++ lib/orwl_auth_sock_functions.c
@@ -22,7 +22,6 @@ DEFINE_AUTH_SOCK_FUNC(auth_sock_insert_peer, uint64_t port) {
   /* mes and addr_t is already in host order */
   h->ep.addr = getpeer(Arg);
   h->ep.port.p = htons((uint16_t)port);
-  report(Arg->srv->info, "inserting peer %s", orwl_endpoint_print(&h->ep));
   orwl_host_connect(h, &Arg->srv->host);
 }
 
