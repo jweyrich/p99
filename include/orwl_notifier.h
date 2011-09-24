9330978c469b2def827754c0ce4368d88bc2c6f1
Jens Gustedt <Jens.Gustedt@loria.fr>
2011-09-25 00:02:32 +0200

    implement the notifier directly, not going through orwl_count

--- include/orwl_notifier.h
+++ include/orwl_notifier.h
@@ -16,34 +16,117 @@
 
 #include "orwl_count.h"
 
+P99_DECLARE_STRUCT(orwl_notifier);
+
 /**
- ** @brief A restriction of ::orwl_count to set flag and notify
- ** other threads that an event has occurred.
+ ** @brief Set a flag and notify other threads that an event has
+ ** occurred.
  **
  ** @see orwl_notifier_set
  ** @see orwl_notifier_block
  ** @see orwl_notifier_verify
  **/
-typedef orwl_count orwl_notifier;
-
-#define ORWL_NOTIFIER_INITIALIZER ORWL_COUNT_INITIALIZER(1)
+struct orwl_notifier {
+  atomic_size_t val;
+#if !defined(HAVE_FUTEX) || !defined(HAVE_ATOMIC)
+  /** @brief A mutex that is used in the non-futex implementations */
+  pthread_mutex_t mut;
+  /** @brief A conditional variable that is used in the non-futex
+   ** implementations */
+  pthread_cond_t cnd;
+#endif
+};
 
 /**
  ** @memberof orwl_notifier
  **/
 inline
-orwl_notifier* orwl_notifier_init(orwl_notifier* notifier) {
-  orwl_count_init(notifier, 1);
-  return notifier;
+orwl_notifier* orwl_notifier_init(orwl_notifier* notifier);
+
+/**
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_destroy(orwl_notifier* notifier);
+
+/**
+ ** @brief Verify if the notification flag has been set. Non blocking.
+ ** @memberof orwl_notifier
+ **/
+inline
+bool orwl_notifier_verify(orwl_notifier* notifier);
+
+/**
+ ** @brief Set the notifier flag and wake up all potential waiters.
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_set(orwl_notifier* notifier);
+
+/**
+ ** @brief Reset the notifier flag.
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_unset(orwl_notifier* notifier);
+
+/**
+ ** @brief Block until the notification flag has been set.
+ **
+ ** Non blocking if the flag is already set.
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_block(orwl_notifier* notifier);
+
+
+#if !defined(HAVE_FUTEX) || !defined(HAVE_ATOMIC)
+# define ORWL_NOTIFIER_INITIALIZER                             \
+{                                                              \
+  .val = 0,                                                    \
+    .mut = PTHREAD_MUTEX_INITIALIZER,                          \
+       .cnd = PTHREAD_COND_INITIALIZER                         \
+}
+#else
+/**
+ ** @brief Initialize an ::orwl_notifier object.
+ **/
+# define ORWL_NOTIFIER_INITIALIZER                             \
+{                                                              \
+  .val = 0,                                                    \
+}
+#endif
+
+/**
+ ** @memberof orwl_notifier
+ **/
+inline
+orwl_notifier* orwl_notifier_init(orwl_notifier* c) {
+  if (c) {
+    c->val = 0;
+#if !defined(HAVE_FUTEX) || !defined(HAVE_ATOMIC)
+    pthread_mutex_init(&c->mut);
+    pthread_cond_init(&c->cnd);
+#endif
+  }
+  return c;
 }
 
 /**
  ** @memberof orwl_notifier
  **/
 inline
-void orwl_notifier_destroy(orwl_notifier* notifier) {
-  orwl_count_destroy(notifier);
+void orwl_notifier_destroy(orwl_notifier* c) {
+  if (c) {
+    c->val = 1;
+#if !defined(HAVE_FUTEX) || !defined(HAVE_ATOMIC)
+    pthread_mutex_destroy(&c->mut);
+    pthread_cond_destroy(&c->cnd);
+#endif
+  }
 }
+
+#if defined(HAVE_ATOMIC)
 
 /**
  ** @brief Verify if the notification flag has been set. Non blocking.
@@ -51,7 +134,8 @@ void orwl_notifier_destroy(orwl_notifier* notifier) {
  **/
 inline
 bool orwl_notifier_verify(orwl_notifier* notifier) {
-  return !orwl_count_value(notifier);
+  assert(notifier);
+  return atomic_load(&notifier->val);
 }
 
 /**
@@ -60,11 +144,12 @@ bool orwl_notifier_verify(orwl_notifier* notifier) {
  **/
 inline
 void orwl_notifier_set(orwl_notifier* notifier) {
-  if (!orwl_notifier_verify(notifier)) {
-    if (orwl_count_dec(notifier, 1)) {
-      report(true, "duplicated notifier set for %p", (void*)notifier);
-    }
-  }
+  atomic_store(&notifier->val, 1);
+#if defined(HAVE_FUTEX)
+  orwl_futex_broadcast((int*)&notifier->val);
+#else
+  pthread_cond_broadcast(&notifier->cnd);
+#endif
 }
 
 /**
@@ -72,11 +157,7 @@ void orwl_notifier_set(orwl_notifier* notifier) {
  **/
 inline
 void orwl_notifier_unset(orwl_notifier* notifier) {
-  if (orwl_notifier_verify(notifier)) {
-    if (orwl_count_inc(notifier, 1)) {
-      report(true, "duplicated notifier unset for %p", (void*)notifier);
-    }
-  }
+  atomic_store(&notifier->val, 0);
 }
 
 /**
@@ -87,7 +168,64 @@ void orwl_notifier_unset(orwl_notifier* notifier) {
  **/
 inline
 void orwl_notifier_block(orwl_notifier* notifier) {
-  orwl_count_wait(notifier);
+  while (!orwl_notifier_verify(notifier)) {
+#if defined(HAVE_FUTEX)
+    ORWL_FUTEX_WAIT(&notifier->val, x, x);
+#else
+    MUTUAL_EXCLUDE(notifier->mut)
+      pthread_cond_wait(&notifier->cnd, &notifier->mut);
+#endif
+  }
 }
+
+#else
+
+/**
+ ** @brief Verify if the notification flag has been set. Non blocking.
+ ** @memberof orwl_notifier
+ **/
+inline
+bool orwl_notifier_verify(orwl_notifier* notifier) {
+  bool ret = false;
+  MUTUAL_EXCLUDE(notifier->mut)
+    ret = notifier->val;
+  return ret;
+}
+
+/**
+ ** @brief Set the notifier flag and wake up all potential waiters.
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_set(orwl_notifier* notifier) {
+  MUTUAL_EXCLUDE(notifier->mut) {
+    notifier->val = 1;
+    pthread_cond_broadcast(&notifier->cnd);
+  }
+}
+
+/**
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_unset(orwl_notifier* notifier) {
+  MUTUAL_EXCLUDE(notifier->mut)
+    notifier->val = 0;
+}
+
+/**
+ ** @brief Block until the notification flag has been set.
+ **
+ ** Non blocking if the flag is already set.
+ ** @memberof orwl_notifier
+ **/
+inline
+void orwl_notifier_block(orwl_notifier* notifier) {
+  MUTUAL_EXCLUDE(notifier->mut)
+    while (!notifier->val)
+      pthread_cond_wait(&notifier->cnd, &notifier->mut);
+}
+
+#endif
 
 #endif
