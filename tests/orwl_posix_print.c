272325ae92d7a6123b1f48b5be0cb24f7372274f
Jens Gustedt <Jens.Gustedt@loria.fr>
2011-09-27 13:42:35 +0200

    split the finish program in two

---
+++ tests/orwl_posix_print.c
@@ -0,0 +1,19 @@
+/* This may look like nonsense, but it really is -*- mode: C -*-             */
+/*                                                                           */
+/* Except of parts copied from previous work and as explicitly stated below, */
+/* the author and copyright holder for this work is                          */
+/* all rights reserved,  2010-2011 Jens Gustedt, INRIA, France               */
+/*                                                                           */
+/* This file is part of the P99 project. You received this file as as        */
+/* part of a confidential agreement and you may generally not                */
+/* redistribute it and/or modify it, unless under the terms as given in      */
+/* the file LICENSE.  It is distributed without any warranty; without        */
+/* even the implied warranty of merchantability or fitness for a             */
+/* particular purpose.                                                       */
+/*                                                                           */
+#include "orwl.h"
+
+int main(int argc, char **argv) {
+  orwl_posix_init();
+  orwl_posix_print();
+}
