2315db64e589a86666aa71f7db625c3ce168e057
Jens Gustedt <Jens.Gustedt@loria.fr>
2012-11-18 13:02:24 +0100

    move futex derivatives from ORWL to P99

---
+++ p99/p99_count.h
@@ -0,0 +1,143 @@
+/* This may look like nonsense, but it really is -*- mode: C -*-              */
+/*                                                                            */
+/* Except for parts copied from previous work and as explicitly stated below, */
+/* the author and copyright holder for this work is                           */
+/* all rights reserved,  2011-2012 Jens Gustedt, INRIA, France                */
+/*                                                                            */
+/* This file is part of the ORWL project. You received this file as as        */
+/* part of a confidential agreement and you may generally not                 */
+/* redistribute it and/or modify it, unless under the terms as given in       */
+/* the file LICENSE.  It is distributed without any warranty; without         */
+/* even the implied warranty of merchantability or fitness for a              */
+/* particular purpose.                                                        */
+/*                                                                            */
+#ifndef P99_COUNT_H
+#define P99_COUNT_H
+
+#include "p99_futex.h"
+
+/**
+ ** @addtogroup library_support Compiler and system support features
+ ** @{
+ **/
+
+/**
+ ** @brief A counter that allows atomic increment and decrement and a
+ ** wait for the value 0.
+ **
+ ** This is implemented more or less efficiently according to the
+ ** features that the platform provides:
+ ** - one that uses atomic operations and futexes for signaling
+ ** - one that mutexes all access and uses a condition variable for
+ **   signaling.
+ **/
+typedef p99_futex p99_count;
+
+
+/**
+ ** @brief Initialize an ::p99_count object.
+ **/
+# define P99_COUNT_INITIALIZER P99_FUTEX_INITIALIZER
+
+/**
+ ** @brief Initialize an ::p99_count object.
+ **/
+p99_inline
+p99_count* p99_count_init(p99_count* p00_c, unsigned p00_v) {
+  return p99_futex_init(p00_c, p00_v);
+}
+
+#define p99_count_init(...) P99_CALL_DEFARG(p99_count_init, 2, __VA_ARGS__)
+#define p99_count_init_defarg_1() 0U
+
+p99_inline
+void p99_count_destroy(p99_count* p00_c) {
+  p99_futex_destroy(p00_c);
+}
+
+/**
+ ** @brief Account the ::p99_count @a COUNT during execution
+ ** of a dependent block or statement.
+ **/
+P99_BLOCK_DOCUMENT
+#define P99_ACCOUNT(COUNT) P99_PROTECTED_BLOCK(p99_count_inc(&COUNT), p99_count_dec(&COUNT))
+
+/**
+ ** @brief increment the counter @a counter atomically by @a p00_hm.
+ ** @return the value of the counter before the increment.
+ ** @remark @a p00_hm defaults to 1 if omitted.
+ ** @related p99_count
+ **/
+p99_inline unsigned p99_count_inc(p99_count volatile* p00_c, unsigned p00_hm) {
+  return p99_futex_add(p00_c, p00_hm, 0U, 0U, 0U, 0U);
+}
+
+#define p99_count_inc(...) P99_CALL_DEFARG(p99_count_inc, 2, __VA_ARGS__)
+#define p99_count_inc_defarg_1() (1U)
+
+/**
+ ** @brief increment the counter @a p00_c atomically by @a p00_hm but only if it is not zero before.
+ ** @return the value of the counter before the increment.
+ ** @remark @a p00_hm defaults to 1 if omitted.
+ ** @related p99_count
+ **/
+P00_FUTEX_INLINE(p99_count_inc_conditionally)
+unsigned p99_count_inc_conditionally(p99_count volatile* p00_c, unsigned p00_hm) {
+  unsigned p00_ret = 0;
+  P99_FUTEX_COMPARE_EXCHANGE(p00_c, p00_act,
+                             /* never wait */
+                             true,
+                             /* only do something if positive */
+                             (!p00_act ? 0U : p00_act + p00_hm),
+                             /* capture the state for return, but
+                                never wakeup and waiters */
+                             ((p00_ret = p00_act), 0), 0);
+  return p00_ret;
+}
+
+#define p99_count_inc_conditionally(...) P99_CALL_DEFARG(p99_count_inc_conditionally, 2, __VA_ARGS__)
+#define p99_count_inc_conditionally_defarg_1() (1U)
+
+/**
+ ** @brief Obtain the value of counter @a p00_c atomically.
+ ** @related p99_count
+ **/
+p99_inline unsigned p99_count_value(p99_count volatile* p00_c) {
+  return p99_futex_load(p00_c);
+}
+
+
+/**
+ ** @brief decrement the counter @a p00_c atomically by @a p00_hm.
+ **
+ ** Also signals eventual waiters if the counter falls to 0.
+ ** @return the value of the counter after the decrement.
+ ** @remark @a p00_hm defaults to 1 if omitted.
+ ** @related p99_count
+ **/
+p99_inline
+unsigned p99_count_dec(p99_count volatile* p00_c, unsigned p00_hm) {
+  unsigned ret = p99_futex_add(p00_c, -(signed)p00_hm, 0U, 1U, 0U, P99_FUTEX_MAX_WAITERS);
+  return ret - p00_hm;
+}
+
+#define p99_count_dec(...) P99_CALL_DEFARG(p99_count_dec, 2, __VA_ARGS__)
+#define p99_count_dec_defarg_1() (1U)
+
+/**
+ ** @brief wait until the p00_c @a counter falls to @c 0.
+ **
+ ** @remark p00_v defaults to @c 0U.
+ ** @related p99_count
+ **/
+P00_FUTEX_INLINE(p99_count_wait)
+void p99_count_wait(p99_count volatile* p00_c) {
+  P99_FUTEX_COMPARE_EXCHANGE(p00_c, p00_act, !p00_act, p00_act, 0U, 0U);
+}
+
+/**
+ ** @}
+ **/
+
+
+#endif
---
+++ p99/p99_notifier.h
@@ -0,0 +1,167 @@
+/* This may look like nonsense, but it really is -*- mode: C -*-              */
+/*                                                                            */
+/* Except for parts copied from previous work and as explicitly stated below, */
+/* the authors and copyright holders for this work are as follows:            */
+/* all rights reserved,  2011 Emmanuel Jeanvoine, INRIA, France               */
+/* all rights reserved,  2011-2012 Jens Gustedt, INRIA, France                */
+/*                                                                            */
+/* This file is part of the P99 project. You received this file as as        */
+/* part of a confidential agreement and you may generally not                 */
+/* redistribute it and/or modify it, unless under the terms as given in       */
+/* the file LICENSE.  It is distributed without any warranty; without         */
+/* even the implied warranty of merchantability or fitness for a              */
+/* particular purpose.                                                        */
+/*                                                                            */
+#ifndef P99_NOTIFIER_H
+#define P99_NOTIFIER_H
+
+#include "p99_futex.h"
+
+/**
+ ** @addtogroup library_support
+ ** @{
+ **/
+
+/**
+ ** @brief Set a flag and notify other threads that an event has
+ ** occurred.
+ **
+ ** Such a variable stores a state and enables threads to wait for a
+ ** certain value of that state.
+ **
+ ** Per default, the state space that is used are just the values @c 0
+ ** and @c 1 but other values that are compatible with @c unsigned or
+ ** @c int, e.g enumeration types, could be used as well.
+ **
+ ** The state with value @c 0 is special and should not be waited for.
+ **
+ ** A typical example could be a herd of threads. Let us suppose that
+ ** we have a main thread that launches such a herd:
+ **
+ ** @code
+ ** p99_notifier all_started = P99_NOTIFIER_INITIALIZER;
+ **
+ ** for (size_t i = 0; i < n; ++i)
+ **   start_my_threads(i, &all_started);
+ **
+ ** p99_notifier_set(&all_started);
+ ** // ... do something clever ...
+ **
+ ** // ... join all the threads ...
+ ** @endcode
+ **
+ ** Then every thread should just block on that notifier at the
+ ** beginning of its function
+ **
+ ** @code
+ ** int my_thread_function(void* arg) {
+ **   p99_notifier* all = arg;
+ **   p99_notifier_block(all);
+ **   ...
+ ** }
+ ** @endcode
+ **
+ ** @see p99_notifier_set
+ ** @see p99_notifier_block
+ ** @see p99_notifier_load
+ ** @see p99_notifier_unset
+ **/
+typedef p99_futex p99_notifier;
+
+/**
+ ** @related p99_notifier
+ **/
+p99_inline
+p99_notifier* p99_notifier_init(p99_notifier* p00_n, unsigned p00_v) {
+  return p99_futex_init(p00_n, p00_v);
+}
+
+#ifndef DOXYGEN
+#define p99_notifier_init(...) P99_CALL_DEFARG(p99_notifier_init, 2, __VA_ARGS__)
+#define p99_notifier_init_defarg_1() 0U
+#endif
+
+
+/**
+ ** @related p99_notifier
+ **/
+p99_inline
+void p99_notifier_destroy(p99_notifier* p00_n) {
+  p99_futex_destroy(p00_n);
+}
+
+/**
+ ** @brief Block until the notification flag has been set to the
+ ** indicated value.
+ **
+ ** Non blocking if the flag is already set to @a p00_v.
+ **
+ ** @remark @a p00_v defaults to @c 1.
+ ** @related p99_notifier
+ **/
+P00_FUTEX_INLINE(p99_notifier_block)
+void p99_notifier_block(p99_notifier volatile* p00_n, unsigned p00_v) {
+  P99_FUTEX_COMPARE_EXCHANGE(p00_n, p00_act,
+                             /* wait for p00_v */
+                             (p00_act == p00_v),
+                             /* never update */
+                             p00_act,
+                             /* never wakeup others */
+                             0u, 0u);
+}
+
+#ifndef DOXYGEN
+#define p99_notifier_block(...) P99_CALL_DEFARG(p99_notifier_block, 2, __VA_ARGS__)
+#define p99_notifier_block_defarg_1() 1U
+#endif
+
+/**
+ ** @brief Initialize an ::p99_notifier object.
+ **/
+# define P99_NOTIFIER_INITIALIZER P99_FUTEX_INITIALIZER(0u)
+
+/**
+ ** @brief Return the value of the notifier. Non blocking.
+ ** @related p99_notifier
+ **/
+p99_inline
+unsigned p99_notifier_load(p99_notifier volatile* p00_n) {
+  return p99_futex_load(p00_n);
+}
+
+/**
+ ** @brief Set the notifier flag to @a p00_v and wake up all potential
+ ** waiters.
+ **
+ ** @remark @a p00_v defaults to @c 1.
+ ** @related p99_notifier
+ **/
+p99_inline
+void p99_notifier_set(p99_notifier volatile* p00_n, unsigned p00_v) {
+  p99_futex_fetch_and_store(p00_n, p00_v, p00_v, 1u, 0u, P99_FUTEX_MAX_WAITERS);
+}
+
+#ifndef DOXYGEN
+#define p99_notifier_set(...) P99_CALL_DEFARG(p99_notifier_set, 2, __VA_ARGS__)
+#define p99_notifier_set_defarg_1() 1U
+#endif
+
+/**
+ ** @brief Unconditionally reset the notifier to value @c 0.
+ **
+ ** This should only be used when it is known that there is no waiter
+ ** for the notifier.
+ **
+ ** @related p99_notifier
+ **/
+p99_inline
+void p99_notifier_unset(p99_notifier volatile* p00_n) {
+  p99_futex_fetch_and_store(p00_n, 0u, 1u, 0u, 0u, 0u);
+}
+
+/**
+ ** @}
+ **/
+
+
+#endif
